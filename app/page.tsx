c36a367fdd7c7bca58180a1fea4e53addc29e962
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-15 19:02:19 +0700

    feat: creating slider section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import { padding } from "@/lib/styles";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      
-    </div>
+    <main className={cn(padding.x, "")}>
+      <section className="flex flex-nowrap items-center gap-0">
+        <figure className="relative h-96 w-2/3">
+          <Image
+            src="/dummy_1.jpg"
+            alt="dummy image"
+            fill
+            className="size-full object-cover object-center"
+          />
+        </figure>
+        <figure className="relative h-96 w-1/3">
+          <Image
+            src="/dummy_3.jpg"
+            alt="dummy image"
+            fill
+            className="size-full object-cover object-center"
+          />
+        </figure>
+      </section>
+    </main>
   );
 }
