264edac0d25797c9fcbec02b7e52bfb1a62a0a00
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-15 19:55:58 +0700

    feat: creating new testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { padding } from "@/lib/styles";
 import Image from "@/components/Image";
+import { Star } from "lucide-react";
 
 export default function Home() {
   return (
@@ -49,6 +50,54 @@ export default function Home() {
             We design premium-quality pieces at budget-friendly prices, perfect for everyday wear.
           </p>
         </blockquote>
+      </section>
+
+      <section className="mt-16">
+        <h4 className="mb-4 text-2xl">Testimonials</h4>
+
+        <fieldset className="flex flex-row flex-nowrap items-center gap-8">
+          {[
+            {
+              stars: 5,
+              name: "Emily Toronto",
+              citizen: "Canada",
+              createdAt: "2023-July-10",
+              comment:
+                "Exceptional quality and stunning design, ideal for any occasion. I'm absolutely in love! ✨👌🏻🤍",
+            },
+            {
+              stars: 5,
+              name: "Mia Calgary",
+              citizen: "Canada",
+              createdAt: "2023-October-02",
+              comment: "Absolutely beautiful! Just as shown, arrived quickly, and they're so charming.",
+            },
+            {
+              stars: 5,
+              name: "Gabriella Olivia",
+              citizen: "New York",
+              createdAt: "2023-September-05",
+              comment:
+                "These earrings are fantastic! I needed secure backs for my second holes, and these are the perfect fit—they look adorable!",
+            },
+          ].map((review, reviewIndex) => (
+            <article key={reviewIndex} className="flex-1">
+              <ul className="flex flex-row gap-1.5">
+                {Array.from({ length: review.stars }).map((_, starIndex) => (
+                  <li key={starIndex}>
+                    <Star className="size-4 fill-amber-500 stroke-amber-500" />
+                  </li>
+                ))}
+              </ul>
+
+              <p className="mt-4 text-sm text-slate-500">{review.comment}</p>
+              <p className="mt-4 text-sm text-slate-500">
+                {review.name}, {review.citizen}
+              </p>
+              <p className="text-xs text-slate-500">{review.createdAt}</p>
+            </article>
+          ))}
+        </fieldset>
       </section>
     </main>
   );
