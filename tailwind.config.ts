de0a9b19ed482acf5dc61d1350356d643d600d3d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-15 17:52:36 +0700

    feat: reinitializing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,34 @@
 import type { Config } from "tailwindcss";
+import twAnimate from "tailwindcss-animate";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        display: ["var(--playfair-display)"],
+        body: ["var(--font-outfit)"],
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    twAnimate,
+    plugin(({ addBase }) => {
+      addBase({
+        "html, body": {
+          scrollBehavior: "smooth",
+          fontFamily: "var(--font-outfit)",
+        },
+        "h1, h2, h3": {
+          fontFamily: "var(--playfair-display)",
+        },
+      });
+    }),
+  ],
 };
 export default config;
