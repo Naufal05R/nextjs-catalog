acea8a512771e7484d9ff08a1c209b2aaf29a443
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-15 18:52:26 +0700

    feat: implement Header component to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Outfit } from "next/font/google";
+import Header from "@/components/Header";
 import "./globals.css";
-import Link from "next/link";
-import { ChevronDown, Mail, Search, ShoppingBag } from "lucide-react";
 
 const playfair_display = Playfair_Display({
   variable: "--font-playfair-display",
@@ -30,57 +29,7 @@ export default function RootLayout({
       <body
         className={`${playfair_display.variable} ${outfit.variable} bg-slate-50 antialiased`}
       >
-        <header className="w-full">
-          <p className="bg-slate-300 py-2 text-center text-xs font-normal">
-            Embrace Timeless Beauty ✨ Your Adventure, Our Adornments 💍 Every
-            Experience Sparkles with Joy
-          </p>
-
-          <nav className="flex flex-row flex-wrap items-center justify-center gap-y-8 px-4 py-8 sm:px-8 lg:px-16 xl:px-32">
-            <Link
-              href="/"
-              className="flex w-1/2 flex-row items-center justify-start text-slate-500 lg:w-1/3"
-            >
-              <Mail className="mr-2.5" />{" "}
-              <span className="text-xs font-light">Get on the list</span>
-            </Link>
-
-            <Link href="/" className="w-full lg:w-1/3">
-              <h1 className="text-center text-3xl font-medium uppercase">
-                App Title
-              </h1>
-            </Link>
-
-            <div className="flex w-1/2 flex-row items-center justify-end text-slate-500 lg:w-1/3">
-              <button className="flex flex-row items-center px-4 py-2">
-                English <ChevronDown className="size-4" />
-              </button>
-              <button className="flex flex-row items-center px-4 py-2">
-                English <ChevronDown className="size-4" />
-              </button>
-              <button className="ml-8 grid place-items-center">
-                <Search className="size-7" />
-              </button>
-              <button className="ml-4 grid place-items-center">
-                <ShoppingBag className="size-7" />
-              </button>
-            </div>
-
-            <ul className="flex w-full flex-row items-center justify-center">
-              {["Home", "Shop", "Guestbook", "About", "Blog", "Contact"].map(
-                (item, index) => (
-                  <Link
-                    key={index}
-                    className="px-4 py-2 font-light text-slate-600 hover:underline"
-                    href="/"
-                  >
-                    {item}
-                  </Link>
-                ),
-              )}
-            </ul>
-          </nav>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
