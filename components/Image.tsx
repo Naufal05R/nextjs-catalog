61331f2e4276fe2dcc7a3feed2e7c6205b520261
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-15 22:14:46 +0700

    feat: update Custom Image component functionality

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,20 +1,25 @@
-import { cn } from "@/lib/utils";
-import NextImage from "next/image";
 import React from "react";
+import NextImage from "next/image";
+import { cn } from "@/lib/utils";
+import { Bag } from "./svg";
 
 interface ImageProps extends Omit<React.ComponentProps<typeof NextImage>, "className"> {
+  FallbackComponent?: React.FC<React.SVGProps<SVGSVGElement>>;
   classNames?: {
     figure?: string;
     image?: string;
   };
 }
 
-const Image = ({ classNames, ...props }: ImageProps) => {
+const Image = ({ FallbackComponent = Bag, classNames, ...props }: ImageProps) => {
   const { figure, image } = classNames ?? {};
 
   return (
-    <figure className={cn("relative", figure)}>
-      <NextImage {...props} className={cn("size-full object-cover object-center", image)} />
+    <figure className={cn("relative overflow-hidden", figure)}>
+      <NextImage {...props} className={cn("z-20 size-full object-cover object-center before:hidden", image)} />
+      <div className="relative z-10 grid size-full place-items-center overflow-hidden bg-slate-300">
+        <FallbackComponent className="absolute aspect-square max-h-full max-w-full" />
+      </div>
     </figure>
   );
 };
