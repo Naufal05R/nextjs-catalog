b945cb2a1970de431505f2e714612b89c56b83df
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-16 14:59:10 +0700

    feat: set image draggable to be false

--- components/Image.tsx
+++ components/Image.tsx
@@ -16,7 +16,11 @@ const Image = ({ FallbackComponent = Bag, classNames, ...props }: ImageProps) =>
 
   return (
     <figure className={cn("relative overflow-hidden", figure)}>
-      <NextImage {...props} className={cn("z-20 size-full object-cover object-center before:hidden", image)} />
+      <NextImage
+        {...props}
+        draggable={false}
+        className={cn("z-20 size-full object-cover object-center before:hidden", image)}
+      />
       <div className="relative z-10 grid size-full place-items-center overflow-hidden bg-slate-300">
         <FallbackComponent className="absolute aspect-square max-h-full max-w-full" />
       </div>
