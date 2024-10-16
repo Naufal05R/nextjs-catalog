61d3e5fbf385a7716101be81b917f7e0e52da2e5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-16 13:26:01 +0700

    feat: fix import pathname

--- components/svg/index.ts
+++ components/svg/index.ts
@@ -3,13 +3,13 @@ export { default as Bag } from "./placeholders/Bag.svg";
 export { default as Shoe } from "./placeholders/Shoe.svg";
 
 // Payment Methods
-export { default as AmericanExpress } from "./payment-methods/AmericanExpress.svg";
-export { default as DinersClub } from "./payment-methods/DinersClub.svg";
-export { default as Discover } from "./payment-methods/Discover.svg";
-export { default as JCB } from "./payment-methods/JCB.svg";
-export { default as Maestro } from "./payment-methods/Maestro.svg";
-export { default as Mastercard } from "./payment-methods/Mastercard.svg";
-export { default as PayPal } from "./payment-methods/PayPal.svg";
-export { default as UnionPay } from "./payment-methods/UnionPay.svg";
-export { default as Venmo } from "./payment-methods/Venmo.svg";
-export { default as Visa } from "./payment-methods/Visa.svg";
+export { default as AmericanExpress } from "./payments/AmericanExpress.svg";
+export { default as DinersClub } from "./payments/DinersClub.svg";
+export { default as Discover } from "./payments/Discover.svg";
+export { default as JCB } from "./payments/JCB.svg";
+export { default as Maestro } from "./payments/Maestro.svg";
+export { default as Mastercard } from "./payments/Mastercard.svg";
+export { default as PayPal } from "./payments/PayPal.svg";
+export { default as UnionPay } from "./payments/UnionPay.svg";
+export { default as Venmo } from "./payments/Venmo.svg";
+export { default as Visa } from "./payments/Visa.svg";
