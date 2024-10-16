6c89155833a4c6fd6dd389ff45a13a2d322bc244
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-16 13:28:04 +0700

    feat: implement Footer component to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,8 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Outfit } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { padding } from "@/lib/styles";
-import {
-  AmericanExpress,
-  DinersClub,
-  Discover,
-  JCB,
-  Maestro,
-  Mastercard,
-  PayPal,
-  UnionPay,
-  Venmo,
-  Visa,
-} from "@/components/svg";
 
 const playfair_display = Playfair_Display({
   variable: "--font-playfair-display",
@@ -44,29 +30,7 @@ export default function RootLayout({
       <body className={`${playfair_display.variable} ${outfit.variable} bg-slate-50 antialiased`}>
         <Header />
         {children}
-        <footer className={cn(padding.x, "flex flex-row items-center justify-between py-24")}>
-          <blockquote className="text-sm text-slate-500">
-            <p>Copyright © {new Date().getFullYear()} LEGENDA PERMATA</p>
-            <Link
-              href="https://naufalrabbani.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:underline"
-            >
-              Developed by Naufal Rabbani
-            </Link>
-          </blockquote>
-
-          <ul className="flex flex-row items-center gap-2">
-            {[AmericanExpress, DinersClub, Discover, JCB, Maestro, Mastercard, PayPal, UnionPay, Venmo, Visa].map(
-              (Icon, index) => (
-                <li key={index}>
-                  <Icon />
-                </li>
-              ),
-            )}
-          </ul>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
