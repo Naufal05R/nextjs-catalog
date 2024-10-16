82027baa9f6eaf105c7c8d6587459254cce7ffc7
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-16 13:26:24 +0700

    feat: creating footer to the app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,21 @@ import type { Metadata } from "next";
 import { Playfair_Display, Outfit } from "next/font/google";
 import Header from "@/components/Header";
 import "./globals.css";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { padding } from "@/lib/styles";
+import {
+  AmericanExpress,
+  DinersClub,
+  Discover,
+  JCB,
+  Maestro,
+  Mastercard,
+  PayPal,
+  UnionPay,
+  Venmo,
+  Visa,
+} from "@/components/svg";
 
 const playfair_display = Playfair_Display({
   variable: "--font-playfair-display",
@@ -26,13 +41,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${playfair_display.variable} ${outfit.variable} bg-slate-50 antialiased`}
-      >
+      <body className={`${playfair_display.variable} ${outfit.variable} bg-slate-50 antialiased`}>
         <Header />
         {children}
-        <footer className="py-24">
+        <footer className={cn(padding.x, "flex flex-row items-center justify-between py-24")}>
+          <blockquote className="text-sm text-slate-500">
+            <p>Copyright © {new Date().getFullYear()} LEGENDA PERMATA</p>
+            <Link
+              href="https://naufalrabbani.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Developed by Naufal Rabbani
+            </Link>
+          </blockquote>
 
+          <ul className="flex flex-row items-center gap-2">
+            {[AmericanExpress, DinersClub, Discover, JCB, Maestro, Mastercard, PayPal, UnionPay, Venmo, Visa].map(
+              (Icon, index) => (
+                <li key={index}>
+                  <Icon />
+                </li>
+              ),
+            )}
+          </ul>
         </footer>
       </body>
     </html>
