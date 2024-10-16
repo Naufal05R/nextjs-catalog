b19135a03051cbb7598ea42e50dcc58d14f00665
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-16 16:16:47 +0700

    feat: implement controller button

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,36 +2,65 @@
 
 import React, { useState } from "react";
 import Image from "./Image";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Carousel = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const length = 8;
 
   return (
-    <article className="max-w-full overflow-scroll no-scrollbar" onClick={() => setCount(count + 1)}>
-      <ul
-        style={{ transform: `translateX(calc(calc(-25% - 4px) * ${count}))` }}
-        className="relative flex max-w-full flex-row items-center gap-4"
-      >
-        {Array.from({ length: 8 }).map((_, collectionIndex) => (
-          <li key={collectionIndex} className="min-w-[calc(25%-12px)]">
-            <Image
-              src={`/dummy_${(collectionIndex % 3) + 1}.jpg`}
-              alt={`dummy_${(collectionIndex % 3) + 1}`}
-              fill
-              classNames={{
-                figure: "w-full aspect-square rounded overflow-hidden",
-              }}
-            />
+    <article className="relative w-full">
+      <div className="absolute -left-8 top-0 z-10 flex aspect-square min-w-[calc(25%-12px)] items-center justify-start">
+        <button className="size-fit rounded-full bg-transparent text-slate-500">
+          <ChevronLeft
+            size={32}
+            strokeWidth={1}
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          />
+        </button>
+      </div>
 
-            {/* <div className="grid aspect-square w-full place-items-center overflow-hidden rounded text-8xl font-bold">
+      <div className="absolute -right-8 top-0 z-10 flex aspect-square min-w-[calc(25%-12px)] items-center justify-end">
+        <button className="size-fit rounded-full bg-transparent text-slate-500">
+          <ChevronRight
+            size={32}
+            strokeWidth={1}
+            onClick={() => {
+              setCount(count - 1);
+            }}
+          />
+        </button>
+      </div>
+
+      <fieldset className="max-w-full overflow-scroll no-scrollbar">
+        <ul
+          style={{ transform: `translateX(calc(calc(25% + 4px) * ${count}))` }}
+          className="relative flex max-w-full flex-row items-center gap-4 transition-transform duration-500"
+        >
+          {Array.from({ length }).map((_, collectionIndex) => (
+            <li key={collectionIndex} className="min-w-[calc(25%-12px)]">
+              <Image
+                src={`/dummy_${(collectionIndex % 3) + 1}.jpg`}
+                alt={`dummy_${(collectionIndex % 3) + 1}`}
+                fill
+                classNames={{
+                  figure: "w-full aspect-square rounded overflow-hidden",
+                }}
+              />
+
+              {/* <div className="grid aspect-square w-full place-items-center overflow-hidden rounded text-8xl font-bold">
                   {collectionIndex + 1}
                 </div> */}
 
-            <p className="mt-4 text-sm text-slate-400">Rp 19,99</p>
-            <p className="mt-0 text-sm text-slate-500">Example Product Title</p>
-          </li>
-        ))}
-      </ul>
+              <p className="mt-4 text-sm text-slate-400">Rp 19,99</p>
+              <p className="mt-0 text-sm text-slate-500">Example Product Title</p>
+            </li>
+          ))}
+        </ul>
+      </fieldset>
     </article>
   );
 };
