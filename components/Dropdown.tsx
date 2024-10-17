fa08babe20ac975d87d7bc2ca30f2205e787ba17
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-17 16:11:25 +0700

    feat: creating new exclusive reusable Dropdown

---
+++ components/Dropdown.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "./ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Mapper from "./Mapper";
+
+interface DropdownProps<T extends Array<{ [key: string]: unknown }>> {
+  data: T;
+  value: keyof T[number];
+  label: Array<keyof T[number]>;
+}
+
+export function Dropdown<T extends Array<{ [key: string]: unknown }>>({ data, value, label }: DropdownProps<T>) {
+  const [position, setPosition] = React.useState("bottom");
+
+  const printValue = (object: T[number]) =>
+    label
+      .reduce((acc, key) => `${acc.toString()} ${object[key]}`, "")
+      .toString()
+      .trim();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="max-h-40 w-56 overflow-scroll">
+        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <Mapper
+            data={data}
+            render={(item) => (
+              <DropdownMenuRadioItem value={`${item[value]}`}>{printValue(item)}</DropdownMenuRadioItem>
+            )}
+          />
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
