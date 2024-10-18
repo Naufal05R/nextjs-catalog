13b82e54859af3d6a2228bf3060ed10c8d88f34a
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-18 09:55:53 +0700

    feat: adjusting layout, adding some paddings

--- app/(root)/(public)/layout.tsx
+++ app/(root)/(public)/layout.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
+import { padding } from "@/lib/styles";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main className={cn(padding.x, "")}>
       <Header />
       {children}
       <Footer />
-    </>
+    </main>
   );
 }
