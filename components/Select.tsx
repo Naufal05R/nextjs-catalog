0fad77a683b4ed099e28dfb35a345e1c4ca394bf
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-18 07:22:20 +0700

    feat: updating select component to more specific in this use-case

--- components/Select.tsx
+++ components/Select.tsx
@@ -9,6 +9,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import Mapper from "./Mapper";
+import { ChevronDown } from "lucide-react";
 
 interface SelectProps<T extends Array<{ [key: string]: unknown }>, V extends keyof T[number]> {
   data: T;
@@ -31,12 +32,22 @@ export function Select<T extends Array<{ [key: string]: unknown }>, V extends ke
 
   return (
     <SelectRoot defaultValue={typeof defaultValue === "string" ? defaultValue : undefined}>
-      <SelectTrigger className="w-[180px]">
+      <SelectTrigger
+        className="w-fit text-slate-500 shadow-none focus:ring-0"
+        icon={<ChevronDown className="ml-2.5 size-4" />}
+      >
         <SelectValue placeholder="Select an option" />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="shadow-none">
         <SelectGroup>
-          <Mapper data={data} render={(item) => <SelectItem value={`${item[value]}`}>{printValue(item)}</SelectItem>} />
+          <Mapper
+            data={data}
+            render={(item) => (
+              <SelectItem value={`${item[value]}`} className="text-slate-400 focus:text-slate-800">
+                {printValue(item)}
+              </SelectItem>
+            )}
+          />
         </SelectGroup>
       </SelectContent>
     </SelectRoot>
