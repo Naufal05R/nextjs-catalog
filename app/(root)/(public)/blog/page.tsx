f7eabd34a9187b9dad3e29b78a19a35acfb5cca7
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 18:27:42 +0700

    feat: initializing new Blog page

---
+++ app/(root)/(public)/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <section className="pt-8">
+      <h4 className="mb-4 text-3xl">Blog</h4>
+    </section>
+  );
+};
+
+export default BlogPage;
+
+// class="mt-8 grid grid-cols-4 sm:grid-cols-8 lg:grid-cols-12 gap-4"
