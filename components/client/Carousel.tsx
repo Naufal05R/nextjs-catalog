aa3d741bb313764baea781a2ec4ccd09a5adaa6b
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:59:03 +0700

    feat: moving Carousel component

--- components/client/Carousel.tsx
+++ components/client/Carousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "./Image";
+import Image from "../Image";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 
