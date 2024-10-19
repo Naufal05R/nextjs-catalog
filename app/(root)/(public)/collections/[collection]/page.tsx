dedd9dea35235e7d86d634dd0ccc1d8114debc0c
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:35:53 +0700

    feat: adding margin-bottom

--- app/(root)/(public)/collections/[collection]/page.tsx
+++ app/(root)/(public)/collections/[collection]/page.tsx
@@ -5,7 +5,7 @@ export default function CollectionPage({ params }: { params: { collection: strin
   return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl capitalize">{params.collection}</h4>
-      <p className="text-sm text-slate-500">
+      <p className="mb-8 text-sm text-slate-500">
         Experience timeless beauty with our Collection, where classic charm meets modern craftsmanship. Each piece is
         meticulously <br /> designed and handcrafted to embody enduring sophistication.
       </p>
