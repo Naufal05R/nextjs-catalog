bcb5dc91a480d6af1f89a2b06a3454ed36e504d0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:39:41 +0700

    feat: updating import path

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
-// fixed const
-export { default as currencies } from "./currency";
-export { default as languages } from "./language";
-export { default as navigations } from "./navigation";
+// permanent const
+export { default as currencies } from "./permanent/currency";
+export { default as languages } from "./permanent/language";
+export { default as navigations } from "./permanent/navigation";
 
 // temporary const
-export { default as collections } from "./collection";
+export { default as collections } from "./temporary/collection";
