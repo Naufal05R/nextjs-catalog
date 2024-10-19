06ac328ce52a4fba59b514dd0474fedad9d89185
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:23:46 +0700

    feat: register collections to constants/index

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,7 @@
+// fixed const
 export { default as currencies } from "./currency";
 export { default as languages } from "./language";
 export { default as navigations } from "./navigation";
+
+// temporary const
+export { default as collections } from "./collection";
