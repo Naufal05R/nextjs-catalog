8e040b8ff8ad39a8e887d602d49c2461afec601f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 10:42:33 +0700

    feat: adding toaster component to the layout

--- app/(root)/(public)/layout.tsx
+++ app/(root)/(public)/layout.tsx
@@ -2,12 +2,14 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { cn } from "@/lib/utils";
 import { padding } from "@/lib/styles";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main className={cn(padding.x, "")}>{children}</main>
+      <Toaster />
       <Footer />
     </>
   );
