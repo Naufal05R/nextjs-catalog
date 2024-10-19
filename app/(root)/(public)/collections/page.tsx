15228982be918d6edd62550ba9dc2776d5ba7f9d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:25:07 +0700

    feat: adjusting section

--- app/(root)/(public)/collections/page.tsx
+++ app/(root)/(public)/collections/page.tsx
@@ -1,4 +1,5 @@
 import Collection from "@/components/Collection";
+import { collections } from "@/constants";
 
 export default function CollectionsPage() {
   return (
@@ -6,23 +7,7 @@ export default function CollectionsPage() {
       <h4 className="mb-4 text-3xl">Our Collections</h4>
 
       <ul className="flex flex-row flex-nowrap items-center gap-4">
-        {[
-          {
-            title: "Ring",
-            description: "Collection of rings",
-            href: "/collections/ring",
-          },
-          {
-            title: "Necklace",
-            description: "Collection of necklaces",
-            href: "/collections/necklace",
-          },
-          {
-            title: "Bracelets",
-            description: "Collection of bracelets",
-            href: "/collections/bracelets",
-          },
-        ].map(({ title, description, href }, collectionIndex) => (
+        {collections.map(({ title, description, href }, collectionIndex) => (
           <Collection key={collectionIndex} title={title} description={description} href={href} />
         ))}
       </ul>
