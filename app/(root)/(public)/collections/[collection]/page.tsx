623d2cae87200a35febe96d6ca4d3521b76931f6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:35:26 +0700

    feat: adding Select component as a filter feature in dynamic collection page

--- app/(root)/(public)/collections/[collection]/page.tsx
+++ app/(root)/(public)/collections/[collection]/page.tsx
@@ -1,11 +1,24 @@
+import { Select } from "@/components/Select";
+import { collections } from "@/constants";
+
 export default function CollectionPage({ params }: { params: { collection: string } }) {
   return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl capitalize">{params.collection}</h4>
       <p className="text-sm text-slate-500">
         Experience timeless beauty with our Collection, where classic charm meets modern craftsmanship. Each piece is
-        meticulously designed and handcrafted to embody enduring sophistication.
+        meticulously <br /> designed and handcrafted to embody enduring sophistication.
       </p>
+
+      <Select
+        data={collections}
+        value={"title"}
+        label={["title"]}
+        placeholder="Filter Collection"
+        classNames={{
+          trigger: "w-48",
+        }}
+      />
     </section>
   );
 }
