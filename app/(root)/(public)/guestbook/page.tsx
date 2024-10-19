4f28db744ed78d9547912e4806310c81c3f0e6d4
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 18:22:51 +0700

    feat: adjusting padding

--- app/(root)/(public)/guestbook/page.tsx
+++ app/(root)/(public)/guestbook/page.tsx
@@ -2,7 +2,7 @@ import { GuestbookForm } from "@/components/client/Form";
 
 export default function GuestbookPage() {
   return (
-    <section className="pt-16">
+    <section className="pt-8">
       <h4 className="mb-4 text-3xl">Guestbook</h4>
       <p>Welcome to our Guest Book! Share your shopping experience and provide valuable feedback here.</p>
 
