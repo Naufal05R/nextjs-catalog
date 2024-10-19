3172b5b09bc49c6a3d48e3592b434cda4316ac29
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:34:56 +0700

    feat: adding key fields to the temporary collection constant

--- constants/collection.ts
+++ constants/collection.ts
@@ -2,16 +2,19 @@ import { defineConst } from "@/lib/utils";
 
 const collections = defineConst([
   {
+    key: "ring",
     title: "Ring",
     description: "Collection of rings",
     href: "/collections/ring",
   },
   {
+    key: "necklace",
     title: "Necklace",
     description: "Collection of necklaces",
     href: "/collections/necklace",
   },
   {
+    key: "bracelets",
     title: "Bracelets",
     description: "Collection of bracelets",
     href: "/collections/bracelets",
