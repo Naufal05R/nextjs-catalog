ce5349f07580ef9888b6568a50d5be0ec699cabe
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:10:04 +0700

    fix: fixing description

--- app/(root)/(public)/collections/page.tsx
+++ app/(root)/(public)/collections/page.tsx
@@ -11,17 +11,17 @@ export default function CollectionsPage() {
         {[
           {
             title: "Ring",
-            description: "Sales words",
+            description: "Collection of rings",
             href: "/collections/ring",
           },
           {
             title: "Necklace",
-            description: "Sales words",
+            description: "Collection of necklaces",
             href: "/collections/necklace",
           },
           {
             title: "Bracelets",
-            description: "Sales words",
+            description: "Collection of bracelets",
             href: "/collections/bracelets",
           },
         ].map(({ title, description, href }, collectionIndex) => (
