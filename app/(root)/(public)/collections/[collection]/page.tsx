2643dd9277a83169d0adbca1cd09d9e8c345ff98
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:13:51 +0700

    feat: initializing collection page description

--- app/(root)/(public)/collections/[collection]/page.tsx
+++ app/(root)/(public)/collections/[collection]/page.tsx
@@ -1,3 +1,11 @@
-export default function CollectionPage() {
-    return <></>
-};
+export default function CollectionPage({ params }: { params: { collection: string } }) {
+  return (
+    <section className="pt-8">
+      <h4 className="mb-4 text-3xl capitalize">{params.collection}</h4>
+      <p className="text-sm text-slate-500f">
+        Experience timeless beauty with our Collection, where classic charm meets modern craftsmanship. Each piece is
+        meticulously designed and handcrafted to embody enduring sophistication.
+      </p>
+    </section>
+  );
+}
