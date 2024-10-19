49e766422ba5bfc80c00c77f57990280a969bd05
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 18:14:40 +0700

    feat: initializing new About Us page and route

---
+++ app/(root)/(public)/about-us/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutUsPage = () => {
+  return (
+    <section className="pt-16">
+      <h4 className="mb-4 text-3xl">About Us</h4>
+
+      
+    </section>
+  );
+};
+
+export default AboutUsPage;
