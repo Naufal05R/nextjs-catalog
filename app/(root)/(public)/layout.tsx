ae810bbe4593942e8182c6b7411939274eb0cb27
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 17:25:27 +0700

    feat: implement Pathname component to the layout

--- app/(root)/(public)/layout.tsx
+++ app/(root)/(public)/layout.tsx
@@ -1,14 +1,19 @@
+import React from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { cn } from "@/lib/utils";
 import { padding } from "@/lib/styles";
 import { Toaster } from "@/components/ui/toaster";
+import { Pathname } from "@/components/client/pathname";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className={cn(padding.x, "")}>{children}</main>
+      <main className={cn(padding.x, "")}>
+        <Pathname />
+        {children}
+      </main>
       <Toaster />
       <Footer />
     </>
