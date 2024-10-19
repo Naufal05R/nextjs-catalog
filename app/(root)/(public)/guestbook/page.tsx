bd2279acfc1801c92bdef06a482d4325cc414590
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-19 21:13:30 +0700

    feat: adding className

--- app/(root)/(public)/guestbook/page.tsx
+++ app/(root)/(public)/guestbook/page.tsx
@@ -4,7 +4,7 @@ export default function GuestbookPage() {
   return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl">Guestbook</h4>
-      <p>Welcome to our Guest Book! Share your shopping experience and provide valuable feedback here.</p>
+      <p className="text-sm text-slate-500">Welcome to our Guest Book! Share your shopping experience and provide valuable feedback here.</p>
 
       <GuestbookForm />
     </section>
