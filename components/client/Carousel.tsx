719ae9a68934360e07c984d95da4fb22ad2c7a32
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-21 16:32:05 +0700

    feat: creating new Carousel component

--- components/client/Carousel.tsx
+++ components/client/Carousel.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "../Image";
+import Fade from "embla-carousel-fade";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { products } from "@/constants";
+import Product from "./Product";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel as CarouselRoot,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  CarouselDot,
+} from "@/components/ui/carousel";
+import Image from "../Image";
 
 interface ControllerProps {
   direction: "left" | "right";
@@ -169,21 +181,8 @@ const Carousel = () => {
           onMouseMove={handleMouseMove}
           onMouseUp={handleMouseUp}
         >
-          {Array.from({ length }).map((_, collectionIndex) => (
-            <li key={collectionIndex} className="min-w-[calc(25%-12px)] select-none">
-              <Image
-                src={`/dummy_${(collectionIndex % 3) + 1}.jpg`}
-                alt={`dummy_${(collectionIndex % 3) + 1}`}
-                sizes="25vw"
-                fill
-                classNames={{
-                  figure: "w-full aspect-square rounded overflow-hidden",
-                }}
-              />
-
-              <p className="mt-4 select-none text-sm text-slate-400">Rp 19,99</p>
-              <p className="mt-0 select-none text-sm text-slate-500">Example Product Title</p>
-            </li>
+          {products.slice(0, length).map((product, collectionIndex) => (
+            <Product key={collectionIndex} {...product} classNames={{ wrapper: "min-w-[calc(25%-12px)]" }} />
           ))}
         </ul>
       </fieldset>
@@ -192,3 +191,44 @@ const Carousel = () => {
 };
 
 export default Carousel;
+
+interface CarouselProps<T extends Array<{ [key: string]: unknown }>> {
+  data: T;
+  el?: React.JSX.Element;
+  classNames?: {
+    root?: string;
+  };
+}
+
+export function CarouselDemo<T extends Array<{ [key: string]: unknown }>>({ data, el, classNames }: CarouselProps<T>) {
+  const { root } = classNames ?? {};
+
+  return (
+    <CarouselRoot className={cn("w-full", root)} opts={{ loop: true }} plugins={[Fade({ active: true })]}>
+      <CarouselContent classNames={{ outer: "h-full", inner: "h-full" }}>
+        {data.map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="h-full">
+              {el ? (
+                el
+              ) : (
+                <Image
+                  src={`/dummy_${(index % 3) + 1}.jpg`}
+                  alt="dummy_1"
+                  fill
+                  classNames={{ figure: "h-96 rounded w-full", image: "select-none" }}
+                />
+              )}
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      <ul className="absolute bottom-5 left-1/2 flex -translate-x-1/2 gap-x-2.5">
+        {data.map((_, index) => (
+          <CarouselDot key={index} index={index} />
+        ))}
+      </ul>
+    </CarouselRoot>
+  );
+}
