b29c4d869877a4b924a1062958bf9966301fd533
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-21 09:07:29 +0700

    feat: adding select none to the wrapper

--- components/client/Product.tsx
+++ components/client/Product.tsx
@@ -14,7 +14,7 @@ const Product = ({ title, price, discount, href, classNames }: ProductProps) => 
   const { wrapper } = classNames ?? {};
 
   return (
-    <Link href={href} className={cn("group relative", wrapper)}>
+    <Link href={href} className={cn("group relative select-none", wrapper)}>
       <Image
         src={`/dummy_1.jpg`}
         alt="dummy_image"
