1715380b96a90e78547fb3dd5f9ce0db0740eb93
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-23 08:54:20 +0700

    feat: refactoring with Mapper component

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "./Image";
 import { cn } from "@/lib/utils";
 import { Collection as Placeholder } from "./svg";
+import Mapper from "./Mapper";
 
 interface CollectionProps {
   title: string;
@@ -14,20 +15,23 @@ const Collection = ({ title, description, href }: CollectionProps) => {
   return (
     <Link href={href} className="flex-1 overflow-hidden rounded">
       <picture className="grid grid-cols-3 grid-rows-2 gap-px">
-        {Array.from({ length: 3 }).map((_, pictureIndex) => (
-          <Image
-            key={pictureIndex}
-            src={`/dummy_${pictureIndex + 1}.jpg`}
-            alt={`dummy_${pictureIndex + 1}`}
-            fill
-            classNames={{
-              figure: cn("aspect-square row-span-2 col-span-2", {
-                "row-span-1 col-span-1": pictureIndex,
-              }),
-            }}
-            FallbackComponent={!pictureIndex ? Placeholder : undefined}
-          />
-        ))}
+        <Mapper
+          data={Array.from({ length: 3 })}
+          render={(_, pictureIndex) => (
+            <Image
+              key={pictureIndex}
+              src={`/dummy_${pictureIndex + 1}.jpg`}
+              alt={`dummy_${pictureIndex + 1}`}
+              fill
+              classNames={{
+                figure: cn("aspect-square row-span-2 col-span-2", {
+                  "row-span-1 col-span-1": pictureIndex,
+                }),
+              }}
+              FallbackComponent={!pictureIndex ? Placeholder : undefined}
+            />
+          )}
+        />
       </picture>
 
       <figcaption className="mt-4 text-lg capitalize">{title}</figcaption>
