c50d9d70eb8355c3d68b28c5183f7fe732f92bc3
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-23 08:32:31 +0700

    feat: refafctoring using Mapper

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,35 +1,28 @@
-"use client"
+"use client";
 
-import { useToast } from "@/hooks/use-toast"
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
+import { useToast } from "@/hooks/use-toast";
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+import Mapper from "../Mapper";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
+      <Mapper
+        data={toasts}
+        render={({ id, title, description, action, ...props }) => (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+              {description && <ToastDescription>{description}</ToastDescription>}
             </div>
             {action}
             <ToastClose />
           </Toast>
-        )
-      })}
+        )}
+      />
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
