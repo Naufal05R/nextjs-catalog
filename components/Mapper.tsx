32ea7e94cd2d40ed15a0ae43156f3f3a4c5e8568
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-23 08:49:52 +0700

    feat: refactoring Mapper component

--- components/Mapper.tsx
+++ components/Mapper.tsx
@@ -1,11 +1,13 @@
 import React, { Children } from "react";
 
-interface MapperProps<T extends Array<{ [key: string]: unknown }>> {
+type Dataset = Array<{ [key: string]: unknown } | unknown>;
+
+interface MapperProps<T extends Dataset> {
   data: T;
   render: (item: T[number], index: number) => React.ReactNode;
 }
 
-const Mapper = <T extends Array<{ [key: string]: unknown }>>({ data, render }: MapperProps<T>) => {
+const Mapper = <T extends Dataset>({ data, render }: MapperProps<T>) => {
   return Children.toArray(data.map((item: T[number], index) => render(item, index)));
 };
 
