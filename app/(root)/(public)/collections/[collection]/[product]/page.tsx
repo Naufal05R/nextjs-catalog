fd4c8771231a21fd3254dd2040d6add507d10dda
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-23 15:50:00 +0700

    fix: fixing carousel in detail product page

--- app/(root)/(public)/collections/[collection]/[product]/page.tsx
+++ app/(root)/(public)/collections/[collection]/[product]/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Image from "@/components/Image";
+import { Carousel } from "@/components/client/Carousel";
+import { products } from "@/constants";
 
 const page = ({ params }: { params: { collection: string; product: string } }) => {
   return (
     <section className="pt-8">
-      <h4 className="mb-4 text-3xl capitalize">{params.collection}</h4>
-      <h4 className="mb-4 text-3xl capitalize">{params.product}</h4>
+      <Carousel
+        data={products}
+        showDots
+        el={
+          <Image
+            src={"/dummy_1.jpg"}
+            alt="dummy_1"
+            fill
+            sizes="10vw"
+            classNames={{ figure: "aspect-square rounded w-full" }}
+          />
+        }
+      />
     </section>
   );
 };
