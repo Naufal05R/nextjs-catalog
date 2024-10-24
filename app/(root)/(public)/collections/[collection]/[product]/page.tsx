08983447ee33a051b3528edae71fefee879df128
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-24 10:47:29 +0700

    feat: implement new adjustment

--- app/(root)/(public)/collections/[collection]/[product]/page.tsx
+++ app/(root)/(public)/collections/[collection]/[product]/page.tsx
@@ -4,22 +4,32 @@ import { Carousel } from "@/components/client/Carousel";
 import { products } from "@/constants";
 
 const page = ({ params }: { params: { collection: string; product: string } }) => {
+  const product = products.find((product) => product.slug === params.product);
+
+  console.log(product);
+
   return (
-    <section className="pt-8">
-      <Carousel
-        data={products}
-        showDots
-        el={
-          <Image
-            src={"/dummy_1.jpg"}
-            alt="dummy_1"
-            fill
-            sizes="10vw"
-            classNames={{ figure: "aspect-square rounded w-full" }}
-          />
-        }
-      />
-    </section>
+    <>
+      <section className="grid grid-cols-12 pt-8">
+        <Carousel
+          data={products}
+          plugins={["fade"]}
+          showDots
+          el={
+            <Image
+              src={"/dummy_1.jpg"}
+              alt="dummy_1"
+              fill
+              sizes="10vw"
+              classNames={{ figure: "aspect-square rounded w-full" }}
+            />
+          }
+          classNames={{ root: "col-span-6" }}
+        />
+      </section>
+
+      <section className="grid grid-cols-12"></section>
+    </>
   );
 };
 
