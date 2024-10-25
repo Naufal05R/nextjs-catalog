f8f3d277218b07783b198dd179cfc1b94e2014f7
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-25 16:10:11 +0700

    feat: update implement

--- app/(root)/(public)/layout.tsx
+++ app/(root)/(public)/layout.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import { cn } from "@/lib/utils";
-import { padding } from "@/lib/styles";
-import { Toaster } from "@/components/ui/toaster";
+
 import { Pathname } from "@/components/client/Pathname";
+import { PublicHeader } from "@/components/Header";
+import { Toaster } from "@/components/ui/toaster";
+import { padding } from "@/lib/styles";
+import { cn } from "@/lib/utils";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      <PublicHeader />
       <main className={cn(padding.x, "")}>
         <Pathname />
         {children}
