46dcc1ac7be449867d066d7b8867e9b97ac1ba94
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-25 17:02:45 +0700

    feat: adding new CategorySchema

--- schema/product.ts
+++ schema/product.ts
@@ -1,4 +1,14 @@
+import { capitalize } from "@/lib/utils";
 import { z } from "zod";
+
+export const CategorySchema = z.object({
+  title: z
+    .string()
+    .min(3, {
+      message: "Title must be at least 3 characters",
+    })
+    .transform((val) => capitalize(val)),
+});
 
 export const ProductSchema = z.object({
   title: z.string().min(5, {
@@ -7,15 +17,12 @@ export const ProductSchema = z.object({
   description: z.string().min(50, {
     message: "Description must be at least 50 characters",
   }),
-  price: z.preprocess(
-    (val) => Number(val),
-    z.number().min(1, { message: "Price must be in positive number" })
-  ),
+  price: z.preprocess((val) => Number(val), z.number().min(1, { message: "Price must be in positive number" })),
   discount: z.preprocess(
     (val) => Number(val),
     z
       .number()
       .multipleOf(0.01)
-      .refine((x) => x * 100 - Math.trunc(x * 100) < Number.EPSILON)
+      .refine((x) => x * 100 - Math.trunc(x * 100) < Number.EPSILON),
   ),
 });
