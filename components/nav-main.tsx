4d151b14f8c24aad51c499595838847d008afc71
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-26 18:18:26 +0700

    feat: updating navmain

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -13,6 +13,10 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
+import { Dialog } from "./server/Dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { createProductCategory } from "@/lib/actions/product.action";
 
 export function NavMain({
   items,
@@ -53,6 +57,23 @@ export function NavMain({
                       </SidebarMenuSubButton>
                     </SidebarMenuSubItem>
                   ))}
+                  <SidebarMenuSubItem>
+                    <Dialog
+                      actionHandler={createProductCategory}
+                      trigger={{ title: `New ${item.title}`, element: SidebarMenuSubButton }}
+                      content={{
+                        title: `Create New ${item.title} Category`,
+                        element: (
+                          <fieldset className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor={item.title} className="text-left">
+                              Title
+                            </Label>
+                            <Input id={item.title} name="title" className="col-span-3" />
+                          </fieldset>
+                        ),
+                      }}
+                    />
+                  </SidebarMenuSubItem>
                 </SidebarMenuSub>
               </CollapsibleContent>
             </SidebarMenuItem>
