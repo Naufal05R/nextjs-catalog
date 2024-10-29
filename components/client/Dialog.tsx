cf5af0b27f38062d03f1e24c9afcd44f49363962
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-29 18:18:36 +0700

    feat: updating dialog component

--- components/client/Dialog.tsx
+++ components/client/Dialog.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useFormState } from "react-dom";
 import { Button } from "@/components/ui/button";
 import {
   Dialog as DialogRoot,
@@ -10,10 +12,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { FormHTMLAttributes, useState } from "react";
+import { createProductCollection } from "@/lib/actions/product.action";
+import { CircleCheck } from "lucide-react";
 
 interface DialogProps {
-  actionHandler: FormHTMLAttributes<HTMLFormElement>["action"];
   trigger: {
     title: string;
     element: React.ElementType;
@@ -25,24 +27,56 @@ interface DialogProps {
   };
 }
 
-export function Dialog({ actionHandler, trigger, content }: DialogProps) {
+export function CreateCollectionDialog({ trigger, content }: DialogProps) {
+  const [collection, formAction, isLoading] = useFormState(createProductCollection, "");
   const [open, setOpen] = useState(false);
+  const [temporaryState, setTemporaryState] = useState<typeof collection>("");
+
+  useEffect(() => {
+    if (collection) {
+      setTemporaryState(collection);
+    }
+  }, [collection]);
 
   return (
-    <DialogRoot>
+    <DialogRoot
+      open={open}
+      onOpenChange={() => {
+        setOpen(!open);
+        if (!open) setTemporaryState("");
+      }}
+    >
       <DialogTrigger asChild>
         <trigger.element>{trigger.title}</trigger.element>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <form action={actionHandler} className="grid gap-4 py-4">
-          <DialogHeader>
-            <DialogTitle className="font-body">{content.title}</DialogTitle>
-            <DialogDescription>{content.description}</DialogDescription>
-          </DialogHeader>
-          {content.element}
-          <DialogFooter>
-            <Button type="submit">Save</Button>
-          </DialogFooter>
+      <DialogContent className="sm:max-w-[425px]" closeButton={!temporaryState}>
+        <form action={formAction} className="grid gap-4 py-4">
+          {temporaryState ? (
+            ""
+          ) : (
+            <DialogHeader>
+              <DialogTitle className="font-body">{content.title}</DialogTitle>
+              <DialogDescription>{content.description}</DialogDescription>
+            </DialogHeader>
+          )}
+          {temporaryState ? (
+            <div className="flex flex-col items-center justify-center gap-8">
+              <CircleCheck size={64} className="text-blue-600" />
+              <h4 className="text-center text-2xl font-semibold text-blue-600">
+                Successfully created new collection called{" "}
+                <span className="bg-blue-600 px-2 text-white">{temporaryState}</span>{" "}
+              </h4>
+            </div>
+          ) : (
+            content.element
+          )}
+          {!temporaryState && (
+            <DialogFooter>
+              <Button disabled={isLoading} type="submit">
+                Save
+              </Button>
+            </DialogFooter>
+          )}
         </form>
       </DialogContent>
     </DialogRoot>
