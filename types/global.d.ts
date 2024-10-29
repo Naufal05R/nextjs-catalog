611c3c0b3de74450ee182749758c01fe32248ab0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-29 19:21:01 +0700

    feat: creating new global type called Replace

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,3 +3,7 @@ type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
 type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
+
+type Replace<S extends string, T extends string, U extends string> = S extends `${infer Start}${T}${infer End}`
+  ? `${Start}${U}${End}`
+  : S;
