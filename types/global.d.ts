34b31b45a282ee19ea71ceb9647ba3c018c95cbc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-29 21:38:48 +0700

    feat: adding new Trim global type

--- types/global.d.ts
+++ types/global.d.ts
@@ -7,3 +7,11 @@ type Prettify<T> = {
 type Replace<S extends string, T extends string, U extends string> = S extends `${infer Start}${T}${infer Rest}`
   ? `${Start}${U}${Replace<Rest, T, U>}`
   : S;
+
+type Trim<S extends string> = S extends ` ${infer Rest}`
+  ? Trim<Rest>
+  : S extends `${infer Rest} `
+    ? Trim<Rest>
+    : S extends `${infer Start} ${infer End}`
+      ? `${Trim<Start>} ${Trim<End>}`
+      : S;
