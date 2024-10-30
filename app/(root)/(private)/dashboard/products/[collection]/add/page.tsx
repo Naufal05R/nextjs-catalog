ff043095e33f3c739c17a99694e880a105975b0b
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-30 18:28:23 +0700

    feat: setting up new create product page

--- app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
@@ -1,3 +1,7 @@
-export default function CreateProductPage() {
-    return <>Create Product</>
-};
+export default function CreateProductPage({ params }: { params: { collection: string } }) {
+  return (
+    <section className="size-full p-4">
+      <h4 className="text-2xl font-semibold capitalize">Create new {params.collection} product</h4>
+    </section>
+  );
+}
