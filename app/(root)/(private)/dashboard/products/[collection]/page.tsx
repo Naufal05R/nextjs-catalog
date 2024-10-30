e9b6206f3826eafd5a3383430392619dc7242562
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-30 17:25:50 +0700

    feat: building product page in blank state

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -1,3 +1,17 @@
-export default function ProductsByCollectionPage() {
-    return <></>
-};
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function ProductsByCollectionPage({ params }: { params: { collection: string } }) {
+  return (
+    <section className="grid size-full place-items-center">
+      <article className="flex flex-col items-center space-y-4">
+        <h4 className="text-center text-3xl font-medium">
+          There&apos;s no products exist <br /> in this collection
+        </h4>
+        <Button asChild>
+          <Link href={`/dashboard/products/${params.collection}/add`}>Create a New Product</Link>
+        </Button>
+      </article>
+    </section>
+  );
+}
