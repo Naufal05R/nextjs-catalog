2c8d1372a7d31b77f3a64ae33bcafc9173f8d767
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-30 19:44:44 +0700

    feat: implement CreateProductForm component in the add page

--- app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
@@ -1,7 +1,11 @@
+import { CreateProductForm } from "@/components/client/Form";
+
 export default function CreateProductPage({ params }: { params: { collection: string } }) {
   return (
     <section className="size-full p-4">
       <h4 className="text-2xl font-semibold capitalize">Create new {params.collection} product</h4>
+
+      <CreateProductForm />
     </section>
   );
 }
