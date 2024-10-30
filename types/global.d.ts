f07ba68d5123a678cfec401dc08df3ce18ce03ee
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-30 07:35:42 +0700

    fix: removing Trim and Replace type for saving memory

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,15 +3,3 @@ type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
 type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
-
-type Replace<S extends string, T extends string, U extends string> = S extends `${infer Start}${T}${infer Rest}`
-  ? `${Start}${U}${Replace<Rest, T, U>}`
-  : S;
-
-type Trim<S extends string> = S extends ` ${infer Rest}`
-  ? Trim<Rest>
-  : S extends `${infer Rest} `
-    ? Trim<Rest>
-    : S extends `${infer Start} ${infer End}`
-      ? `${Trim<Start>} ${Trim<End>}`
-      : S;
