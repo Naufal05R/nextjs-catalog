2dfd1b752b0976e662fcbfa6ea9e7ac7bcb96816
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-31 08:14:25 +0700

    feat: updating zod schema

--- schema/collection.ts
+++ schema/collection.ts
@@ -8,8 +8,8 @@ export const CollectionSchema = z.object({
     .min(3, {
       message: "Title must be at least 3 characters",
     })
-    .max(64, {
-      message: "Title must be less than 64 characters",
+    .max(15, {
+      message: "Title must be less than 15 characters",
     })
     .transform((val) => capitalize(val)),
   slug: z
@@ -17,10 +17,18 @@ export const CollectionSchema = z.object({
     .min(3, {
       message: "Slug must be at least 3 characters",
     })
-    .max(64, {
-      message: "Slug must be less than 64 characters",
+    .max(15, {
+      message: "Slug must be less than 15 characters",
     })
     .transform((val) => slugify(val)),
+  description: z
+    .string()
+    .min(7, {
+      message: "Description must be at least 7 characters",
+    })
+    .max(31, {
+      message: "Title must be less than 31 characters",
+    }),
 });
 
 export const CollectionFormSchema = CollectionSchema.pick({
