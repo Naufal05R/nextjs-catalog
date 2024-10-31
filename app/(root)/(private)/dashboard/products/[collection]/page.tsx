d6fbbd29faed0d4bbf4e02dfd79e0399b8c7fe7d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-31 17:30:30 +0700

    feat: make some adjustment to the page

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -5,8 +5,8 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { getCollection } from "@/lib/actions/collection.action";
 import { getProducts } from "@/lib/actions/product.action";
-import { formatPrice } from "@/lib/utils";
-import { Archive, Pencil, PlusCircle } from "lucide-react";
+import { cn, formatPrice } from "@/lib/utils";
+import { Archive, Grid2x2Plus, Pencil } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
@@ -20,12 +20,12 @@ export default async function ProductsByCollectionPage({ params }: { params: { c
   return (
     <section className="grid size-full place-items-start">
       {!!products?.length ? (
-        <ul className="grid w-full grid-cols-12 gap-4 px-4">
+        <ul className="grid w-full grid-cols-12 gap-4 px-4 pb-16">
           <Mapper
             data={products}
             render={({ title, price, state, discount, description, isReady }, i) =>
               isReady && (
-                <Card className="col-span-12 h-fit overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
+                <Card className="col-span-12 h-fit min-h-full overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
                   <CardHeader>
                     <Image
                       src={`/dummy_${(i % 3) + 1}.jpg`}
@@ -55,21 +55,25 @@ export default async function ProductsByCollectionPage({ params }: { params: { c
                     </div>
                   </CardContent>
 
-                  <CardFooter className="justify-between">
-                    <Badge variant={"outline"} className="border-rose-200 text-rose-500">
-                      {discount}% OFF
-                    </Badge>
+                  <CardFooter className={cn("flex-1 justify-end", { "justify-between": !!discount })}>
+                    {!!discount && (
+                      <Badge variant={"outline"} className="border-rose-200 text-rose-500">
+                        {discount}% OFF
+                      </Badge>
+                    )}
                     <div className="flex flex-col">
-                      <CardDescription className="w-full text-right font-semibold line-through">
-                        Rp. {formatPrice(price)}
-                      </CardDescription>
+                      {!!discount && (
+                        <CardDescription className="w-full text-right font-semibold line-through">
+                          Rp. {formatPrice(price)}
+                        </CardDescription>
+                      )}
                       <CardTitle className="w-full text-right font-semibold">
                         Rp. {formatPrice(price * (1 - Math.min(Math.abs(discount ?? 0), 100) / 100))}
                       </CardTitle>
                     </div>
                   </CardFooter>
 
-                  <CardFooter className="gap-4">
+                  <CardFooter className="mt-auto gap-4">
                     <Button className="flex-1">
                       <Archive />
                       Archive
@@ -84,12 +88,34 @@ export default async function ProductsByCollectionPage({ params }: { params: { c
             }
           />
 
-          <Card className="col-span-12 grid h-full place-items-center overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
+          <Card className="relative col-span-12 h-fit min-h-full overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
+            <CardHeader className="invisible">
+              <figure className="aspect-video w-full rounded-md"></figure>
+            </CardHeader>
+            <CardContent className="invisible">
+              <CardTitle className="text-lg font-semibold">Invisible Title</CardTitle>
+              <CardDescription className="mb-2 line-clamp-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur unde, quidem amet labore quasi
+                laboriosam placeat ut repellendus. Voluptatum velit optio repellat, cum tempora fuga harum aperiam
+                provident voluptate dolorum! Architecto, harum?
+              </CardDescription>
+              <Badge>Invisible Badge</Badge>
+            </CardContent>
+            <CardFooter className="invisible">
+              <div className="flex flex-col">
+                <CardDescription className="w-full">Invisible Price</CardDescription>
+                <CardTitle className="w-full">Invisible Price</CardTitle>
+              </div>
+            </CardFooter>
+            <CardFooter className="invisible">
+              <Button className="flex-1">Invisible Button</Button>
+            </CardFooter>
+
             <Link
               href={`/dashboard/products/${params.collection}/add`}
-              className="flex size-full flex-col items-center justify-center"
+              className="absolute inset-0 flex size-full flex-col items-center justify-center"
             >
-              <PlusCircle className="mx-auto" size={64} />
+              <Grid2x2Plus className="mx-auto" size={64} />
               <CardTitle className="mt-4 line-clamp-1 whitespace-nowrap text-xl font-semibold">
                 Create new <span className="capitalize">{params.collection}</span>
               </CardTitle>
