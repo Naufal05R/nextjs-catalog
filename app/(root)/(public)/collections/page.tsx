0f3c6d199455575b5fc81b70adf9e025364a0651
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-31 08:40:38 +0700

    feat: updating collections page

--- app/(root)/(public)/collections/page.tsx
+++ app/(root)/(public)/collections/page.tsx
@@ -1,16 +1,26 @@
 import Collection from "@/components/server/Collection";
 import Mapper from "@/components/Mapper";
 import { collections } from "@/constants";
+import { getAllCollection } from "@/lib/actions/collection.action";
 
-export default function CollectionsPage() {
+export default async function CollectionsPage() {
+  const allCollections = await getAllCollection();
+
   return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl">Our Collections</h4>
 
-      <ul className="flex flex-row flex-nowrap items-center gap-4">
+      <ul className="grid grid-cols-12 items-center gap-4">
         <Mapper
-          data={collections}
-          render={({ title, description, href }) => <Collection title={title} description={description} href={href} />}
+          data={allCollections!.slice(0, 3) ?? collections}
+          render={({ title, description, slug }) => (
+            <Collection
+              title={title}
+              description={description ?? ""}
+              href={`/collections/${slug}`}
+              classNames={{ wrapper: "col-span-4" }}
+            />
+          )}
         />
       </ul>
     </section>
