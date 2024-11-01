174899110a5ea5b15a162966603e52ea7f6fa1cd
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-01 13:53:16 +0700

    feat: creating new subcollection schema

---
+++ schema/subcollection.ts
@@ -0,0 +1,42 @@
+import { capitalize, slugify } from "@/lib/utils";
+import { z } from "zod";
+import { ProductSchema } from "./product";
+
+export const SubcollectionSchema = z.object({
+  id: z.string().cuid(),
+  title: z
+    .string()
+    .min(3, {
+      message: "Title must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Title must be less than 15 characters",
+    })
+    .transform((val) => capitalize(val)),
+  slug: z
+    .string()
+    .min(3, {
+      message: "Slug must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Slug must be less than 15 characters",
+    })
+    .transform((val) => slugify(val)),
+  description: z
+    .string()
+    .min(7, {
+      message: "Description must be at least 7 characters",
+    })
+    .max(31, {
+      message: "Description must be less than 31 characters",
+    }),
+  products: z.array(ProductSchema.required()).optional(),
+  collectionId: z.string().cuid(),
+});
+
+export const SubcollectionFormSchema = SubcollectionSchema.pick({
+  title: true,
+  description: true,
+});
+
+export const SubcollectionsSchema = z.array(SubcollectionSchema);
