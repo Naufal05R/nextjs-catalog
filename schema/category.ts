e49e496493216f532bdf491859eb7c6741da4970
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-01 13:56:05 +0700

    feat: creating new category schema

---
+++ schema/category.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { ProductSchema } from "./product";
+import { capitalize, slugify } from "@/lib/utils";
+
+export const CategorySchema = z.object({
+  id: z.string().cuid(),
+  title: z
+    .string()
+    .min(3, {
+      message: "Title must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Title must be less than 15 characters",
+    })
+    .transform((val) => capitalize(val)),
+  slug: z
+    .string()
+    .min(3, {
+      message: "Slug must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Slug must be less than 15 characters",
+    })
+    .transform((val) => slugify(val)),
+  description: z
+    .string()
+    .min(7, {
+      message: "Description must be at least 7 characters",
+    })
+    .max(31, {
+      message: "Description must be less than 31 characters",
+    }),
+  products: z.array(ProductSchema.required()),
+});
+
+export const CategoryFormSchema = CategorySchema.pick({
+  title: true,
+  description: true,
+});
