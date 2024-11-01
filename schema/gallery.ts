81b13e19662f536307db70fc5e2ab6b89b45e9ed
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-01 13:58:33 +0700

    feat: creating new gallery schema

---
+++ schema/gallery.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { ImageSchema } from "./image";
+import { capitalize, slugify } from "@/lib/utils";
+
+export const GallerySchema = z.object({
+  id: z.string().cuid(),
+  title: z
+    .string()
+    .min(3, {
+      message: "Title must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Title must be less than 15 characters",
+    })
+    .transform((val) => capitalize(val)),
+  slug: z
+    .string()
+    .min(3, {
+      message: "Slug must be at least 3 characters",
+    })
+    .max(15, {
+      message: "Slug must be less than 15 characters",
+    })
+    .transform((val) => slugify(val)),
+  images: z.array(ImageSchema.required()),
+  productId: z.string().cuid(),
+});
