f9091e95f8706e37bc953c072d346353e5566848
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-01 21:47:33 +0700

    feat: updating Mapper path

--- components/Select.tsx
+++ components/Select.tsx
@@ -8,7 +8,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import Mapper from "./Mapper";
+import Mapper from "./server/Mapper";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Dataset } from "@/types/data";
