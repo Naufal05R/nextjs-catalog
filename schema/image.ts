c72daa8a69c314fda60f8e1cf1576c3cc433d7f2
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-02 18:26:41 +0700

    feat: updating image schema

--- schema/image.ts
+++ schema/image.ts
@@ -2,8 +2,15 @@ import { z } from "zod";
 import { capitalize, slugify } from "@/lib/utils";
 
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
-export const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+const ACCEPTED_VIDEO_MIME_TYPES = ["video/mp4", "video/mpeg", "video/avi", "video/wmv"];
+
 const ACCEPTED_IMAGE_TYPES = ["jpeg", "jpg", "png", "webp"];
+const ACCEPTED_VIDEO_TYPES = ["mp4", "mpeg", "avi", "wmv"];
+
+export const ACCEPTED_MEDIA_MIME_TYPES = ACCEPTED_IMAGE_MIME_TYPES.concat(ACCEPTED_VIDEO_MIME_TYPES);
+export const ACCEPTED_MEDIA_TYPES = ACCEPTED_IMAGE_TYPES.concat(ACCEPTED_VIDEO_TYPES);
 
 export const ImageSchema = z.object({
   id: z.string().cuid(),
@@ -38,8 +45,8 @@ export const ImageSchema = z.object({
   image: z
     .instanceof(File)
     .refine((file) => file && file.size <= MAX_FILE_SIZE, { message: "Max image size is 5MB." })
-    .refine((file) => file && ACCEPTED_IMAGE_MIME_TYPES.includes(file.type), {
-      message: `Only "${ACCEPTED_IMAGE_TYPES.join('", "')}" formats are supported.`,
+    .refine((file) => file && ACCEPTED_MEDIA_MIME_TYPES.includes(file.type), {
+      message: `Only "${ACCEPTED_MEDIA_TYPES.join('", "')}" formats are supported.`,
     }),
   galleryId: z.string().cuid(),
 });
