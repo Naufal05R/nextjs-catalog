6ecf1eeab72a78ad7f2ec2dbf0f6eb90b55e9c4e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-02 21:45:15 +0700

    feat: simplifying Media component

--- components/server/Media.tsx
+++ components/server/Media.tsx
@@ -3,15 +3,8 @@ import NextImage from "next/image";
 import { cn } from "@/lib/utils";
 import { Bag } from "../svg";
 
-interface ImageProps extends Omit<React.ComponentProps<typeof NextImage>, "className"> {
-  type: `image${string}`;
-}
-interface VideoProps
-  extends Omit<React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>, "className"> {
-  type: `video${string}`;
-}
-
 interface ComponentProps {
+  type: string;
   FallbackComponent?: React.FC<React.SVGProps<SVGSVGElement>>;
   classNames?: {
     figure?: string;
@@ -23,23 +16,27 @@ interface ComponentProps {
   };
 }
 
-type MediaProps<T extends string> = ComponentProps &
-  (T extends `image${string}` ? ImageProps : T extends `video${string}` ? VideoProps : never);
+type ImageProps = Omit<React.ComponentProps<typeof NextImage>, "className">;
+type VideoProps = Omit<
+  React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>,
+  "className"
+>;
+type MediaProps = ComponentProps & (ImageProps | VideoProps);
 
-const Media = <T extends string>({ FallbackComponent = Bag, classNames, ...props }: MediaProps<T>) => {
+const Media = ({ FallbackComponent = Bag, classNames, ...props }: MediaProps) => {
   const { figure, media: image, fallback } = classNames ?? {};
   const { type, src } = props;
 
   return (
     <figure className={cn("relative overflow-hidden", figure)}>
-      {type.startsWith("image") && src && (
+      {type.startsWith("image/") && src && (
         <NextImage
-          {...(props as unknown as ImageProps)}
+          {...(props as ImageProps)}
           draggable={false}
           className={cn("z-20 size-full object-cover object-center before:hidden", image)}
         />
       )}
-      {type.startsWith("video") && (
+      {type.startsWith("video/") && (
         <video {...(props as VideoProps)} className={cn("z-20 size-full object-cover object-center", image)} />
       )}
       <div
