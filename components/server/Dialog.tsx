b76fe8fd05371de3a776e23c7214205231c8957b
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-02 09:13:54 +0700

    feat: creating new Dialog Component

---
+++ components/server/Dialog.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog as DialogRoot,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import React from "react";
+
+interface DialogProps {
+  header: {
+    title: string;
+    description?: string;
+  };
+  element: {
+    trigger: React.JSX.Element;
+    body: React.JSX.Element;
+  };
+  footer?: boolean;
+}
+
+export function Dialog({ header, element, footer = false }: DialogProps) {
+  return (
+    <DialogRoot>
+      <DialogTrigger asChild>{element.trigger}</DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>{header.title}</DialogTitle>
+          <DialogDescription>{header.description}</DialogDescription>
+        </DialogHeader>
+        
+        {element.body}
+
+        {footer && (
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    </DialogRoot>
+  );
+}
