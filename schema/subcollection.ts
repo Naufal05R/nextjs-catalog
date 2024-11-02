9e193163584c9ce27d5b03b3ec5d12d56349f52e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-02 07:10:53 +0700

    feat: updating subcollection schema

--- schema/subcollection.ts
+++ schema/subcollection.ts
@@ -9,8 +9,8 @@ export const SubcollectionSchema = z.object({
     .min(3, {
       message: "Title must be at least 3 characters",
     })
-    .max(15, {
-      message: "Title must be less than 15 characters",
+    .max(31, {
+      message: "Title must be less than 31 characters",
     })
     .transform((val) => capitalize(val)),
   slug: z
@@ -18,8 +18,8 @@ export const SubcollectionSchema = z.object({
     .min(3, {
       message: "Slug must be at least 3 characters",
     })
-    .max(15, {
-      message: "Slug must be less than 15 characters",
+    .max(31, {
+      message: "Slug must be less than 31 characters",
     })
     .transform((val) => slugify(val)),
   description: z
@@ -27,8 +27,8 @@ export const SubcollectionSchema = z.object({
     .min(7, {
       message: "Description must be at least 7 characters",
     })
-    .max(31, {
-      message: "Description must be less than 31 characters",
+    .max(63, {
+      message: "Description must be less than 63 characters",
     }),
   products: z.array(ProductSchema.required()).optional(),
   collectionId: z.string().cuid(),
