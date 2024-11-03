432b77f2f2afd655b23c3f7fc3f43494097d8527
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-03 07:39:03 +0700

    feat: modifying media schema

--- schema/media.ts
+++ schema/media.ts
@@ -3,14 +3,14 @@ import { capitalize, slugify } from "@/lib/utils";
 
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
 
-const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
-const ACCEPTED_VIDEO_MIME_TYPES = ["video/mp4", "video/mpeg", "video/avi", "video/wmv"];
+const ACCEPTED_IMAGE_MIME_EXTS = ["image/jpeg", "image/jpg", "image/png", "image/webp"] as const;
+const ACCEPTED_VIDEO_MIME_EXTS = ["video/mp4", "video/mpeg", "video/avi", "video/wmv"] as const;
 
-const ACCEPTED_IMAGE_TYPES = ["jpeg", "jpg", "png", "webp"];
-const ACCEPTED_VIDEO_TYPES = ["mp4", "mpeg", "avi", "wmv"];
+const ACCEPTED_IMAGE_EXTS = ["jpeg", "jpg", "png", "webp"] as const;
+const ACCEPTED_VIDEO_EXTS = ["mp4", "mpeg", "avi", "wmv"] as const;
 
-export const ACCEPTED_MEDIA_MIME_TYPES = ACCEPTED_IMAGE_MIME_TYPES.concat(ACCEPTED_VIDEO_MIME_TYPES);
-export const ACCEPTED_MEDIA_TYPES = ACCEPTED_IMAGE_TYPES.concat(ACCEPTED_VIDEO_TYPES);
+export const ACCEPTED_MEDIA_MIME_TYPES = [...ACCEPTED_IMAGE_MIME_EXTS, ...ACCEPTED_VIDEO_MIME_EXTS];
+export const ACCEPTED_MEDIA_TYPES = [...ACCEPTED_IMAGE_EXTS, ...ACCEPTED_VIDEO_EXTS];
 
 export const MediaSchema = z.object({
   id: z.string().cuid(),
@@ -45,7 +45,7 @@ export const MediaSchema = z.object({
   media: z
     .instanceof(File)
     .refine((file) => file && file.size <= MAX_FILE_SIZE, { message: "Max image size is 5MB." })
-    .refine((file) => file && ACCEPTED_MEDIA_MIME_TYPES.includes(file.type), {
+    .refine((file) => file && Array.from<string>(ACCEPTED_MEDIA_MIME_TYPES).includes(file.type), {
       message: `Only "${ACCEPTED_MEDIA_TYPES.join('", "')}" formats are supported.`,
     }),
   galleryId: z.string().cuid(),
