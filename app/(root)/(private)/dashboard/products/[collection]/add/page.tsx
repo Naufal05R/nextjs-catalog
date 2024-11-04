8bcb88c3ca557195635fcb4095f7755c0bd777ea
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-04 14:20:16 +0700

    feat: implement update

--- app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
@@ -1,11 +1,14 @@
 import { CreateProductForm } from "@/components/client/Form";
+import { getAllCategory } from "@/lib/actions/category.action";
 
-export default function CreateProductPage({ params }: { params: { collection: string } }) {
+export default async function CreateProductPage({ params }: { params: { collection: string } }) {
+  const categories = await getAllCategory();
+
   return (
     <section className="size-full p-4">
       <h4 className="text-2xl font-semibold capitalize">Create new {params.collection} product</h4>
 
-      <CreateProductForm collection={params.collection} />
+      <CreateProductForm collection={params.collection} categories={categories ?? []} />
     </section>
   );
 }
