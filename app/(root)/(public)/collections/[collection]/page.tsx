c7fc92a0a87778809e7b93110064e9f486bc6a89
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-05 18:17:39 +0700

    feat: updating collection page

--- app/(root)/(public)/collections/[collection]/page.tsx
+++ app/(root)/(public)/collections/[collection]/page.tsx
@@ -2,18 +2,24 @@ import Product from "@/components/client/Product";
 import Mapper from "@/components/server/Mapper";
 import { Select } from "@/components/server/Select";
 import { collections } from "@/constants";
+import { getCollection } from "@/lib/actions/collection.action";
 import { getAllProduct } from "@/lib/actions/product.action";
 
 export default async function CollectionPage({ params }: { params: { collection: string } }) {
-  const allProducts = await getAllProduct();
+  const collection = await getCollection(params.collection, "slug").then((collection) => {
+    if (collection) {
+      return collection;
+    } else {
+      throw new Error("Collection not found");
+    }
+  });
+
+  const allProducts = await getAllProduct(collection.id, "collectionId");
 
   return (
     <section className="pt-8">
-      <h4 className="mb-4 text-3xl capitalize">{params.collection}</h4>
-      <p className="mb-8 text-sm text-slate-500">
-        Experience timeless beauty with our Collection, where classic charm meets modern craftsmanship. Each piece is
-        meticulously <br /> designed and handcrafted to embody enduring sophistication.
-      </p>
+      <h4 className="mb-4 text-3xl capitalize">{collection.title}</h4>
+      <p className="mb-8 text-sm text-slate-500">{collection.description}</p>
 
       <Select
         data={collections}
@@ -30,7 +36,12 @@ export default async function CollectionPage({ params }: { params: { collection:
           <Mapper
             data={allProducts}
             render={(product) => (
-              <Product {...product} collection={params.collection} classNames={{ wrapper: "col-span-3" }} />
+              <Product
+                {...product}
+                collection={params.collection}
+                thumbnail={product.gallery!.medias[0].name}
+                classNames={{ wrapper: "col-span-3" }}
+              />
             )}
           />
         )}
