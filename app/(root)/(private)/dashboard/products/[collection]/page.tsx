10521e2c03ce982293902bcb4b2bf72f3bd74b48
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-05 14:25:20 +0700

    feat: implement new dynamic product

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Mapper from "@/components/server/Mapper";
-import { Image } from "@/components/server/Media";
+import { Image as ImageComponent } from "@/components/server/Media";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -9,6 +9,8 @@ import { getProducts } from "@/lib/actions/product.action";
 import { cn, formatPrice } from "@/lib/utils";
 import { Archive, Grid2x2Plus, Pencil } from "lucide-react";
 import { notFound } from "next/navigation";
+import { getImageSrc } from "@/lib/actions/image.action";
+import { DashbaordProductCard } from "@/components/server/Product";
 
 export default async function ProductsByCollectionPage({ params }: { params: { collection: string } }) {
   const collectionId = await getCollection(params.collection, "slug").then((collection) => collection?.id);
@@ -23,69 +25,17 @@ export default async function ProductsByCollectionPage({ params }: { params: { c
         <ul className="grid w-full grid-cols-12 gap-4 px-4 pb-16">
           <Mapper
             data={products}
-            render={({ title, price, state, discount, description, isReady }, i) =>
-              isReady && (
-                <Card className="col-span-12 h-fit min-h-full overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
-                  <CardHeader>
-                    <Image
-                      src={`/dummy_${(i % 3) + 1}.jpg`}
-                      alt="dummy_image"
-                      fill
-                      sizes="25vw"
-                      classNames={{ figure: "w-full aspect-video rounded-md" }}
-                    />
-                  </CardHeader>
-
-                  <CardContent>
-                    <CardTitle className="line-clamp-1 whitespace-nowrap text-lg font-semibold">{title}</CardTitle>
-                    <CardDescription className="mb-2 line-clamp-3">{description}</CardDescription>
-                    <div className="ml-auto flex w-fit gap-2">
-                      <Badge
-                        variant={"secondary"}
-                        className="bg-sky-100 text-sky-900 hover:bg-sky-100/80 dark:bg-sky-800 dark:text-sky-50 dark:hover:bg-sky-800/80"
-                      >
-                        Sapphire
-                      </Badge>
-                      <Badge
-                        variant={"secondary"}
-                        className="bg-teal-100 text-teal-900 hover:bg-teal-100/80 dark:bg-teal-800 dark:text-teal-50 dark:hover:bg-teal-800/80"
-                      >
-                        {state} Origin
-                      </Badge>
-                    </div>
-                  </CardContent>
-
-                  <CardFooter className={cn("flex-1 justify-end", { "justify-between": !!discount })}>
-                    {!!discount && (
-                      <Badge variant={"outline"} className="border-rose-200 text-rose-500">
-                        {discount}% OFF
-                      </Badge>
-                    )}
-                    <div className="flex flex-col">
-                      {!!discount && (
-                        <CardDescription className="w-full text-right font-semibold line-through">
-                          Rp. {formatPrice(price)}
-                        </CardDescription>
-                      )}
-                      <CardTitle className="w-full text-right font-semibold">
-                        Rp. {formatPrice(price * (1 - Math.min(Math.abs(discount ?? 0), 100) / 100))}
-                      </CardTitle>
-                    </div>
-                  </CardFooter>
-
-                  <CardFooter className="mt-auto gap-4">
-                    <Button className="flex-1">
-                      <Archive />
-                      Archive
-                    </Button>
-                    <Button className="flex-1">
-                      <Pencil />
-                      Edit
-                    </Button>
-                  </CardFooter>
-                </Card>
-              )
-            }
+            render={({ gallery, ...product }) => {
+              return (
+                gallery && (
+                  <DashbaordProductCard
+                    {...product}
+                    collection={params.collection}
+                    thumbnail={gallery.medias[0].name}
+                  />
+                )
+              );
+            }}
           />
 
           <Card className="relative col-span-12 h-fit min-h-full overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
