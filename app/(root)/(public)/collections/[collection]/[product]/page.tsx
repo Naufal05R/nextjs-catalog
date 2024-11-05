f9b443ee2b1daa9e8e43a021fc5c90354d014335
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-05 15:21:57 +0700

    feat: updating page name

--- app/(root)/(public)/collections/[collection]/[product]/page.tsx
+++ app/(root)/(public)/collections/[collection]/[product]/page.tsx
@@ -8,7 +8,7 @@ import { notFound } from "next/navigation";
 import { Whatsapp } from "@/components/svg";
 import Link from "next/link";
 
-const page = async ({ params }: { params: { collection: string; product: string } }) => {
+const DetailProductPage = async ({ params }: { params: { collection: string; product: string } }) => {
   const product = await prisma.product.findUnique({
     where: { slug: params.product },
     include: { gallery: { include: { medias: true } } },
@@ -119,4 +119,4 @@ const page = async ({ params }: { params: { collection: string; product: string 
   );
 };
 
-export default page;
+export default DetailProductPage;
