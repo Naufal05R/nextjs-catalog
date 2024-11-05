8b1ce0317f8f7284a3444c31098d74eb71559628
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-05 14:41:27 +0700

    feat: updating image server action

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -7,11 +7,21 @@ const SERVER_HOST = process.env.MINIO_SERVER_HOST;
 const SERVER_PORT = process.env.MINIO_SERVER_PORT;
 const SECURE_URL = process.env.MINIO_SECURE_URL;
 
-export const getImageSrc = async ({ collection, name }: { collection: string; name: string }) => {
+export const getImageSrc = async ({
+  collection,
+  product,
+  name,
+}: {
+  collection: string;
+  product: string;
+  name: string;
+}) => {
   try {
     const protocol = SECURE_URL === "true" ? "https" : "http";
 
-    const res = await fetch(`${protocol}://${SERVER_HOST}:${SERVER_PORT}/${BUCKET_NAME}/${collection}/${name}`);
+    const res = await fetch(
+      `${protocol}://${SERVER_HOST}:${SERVER_PORT}/${BUCKET_NAME}/${collection}/${product}/${name}`,
+    );
     const src = await res.text();
 
     return src;
