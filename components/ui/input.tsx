fd32d0150a2a658a6c262b5351bc7ea3a049e0f1
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-07 18:16:57 +0700

    feat: adjusting input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,24 +1,27 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
-// export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-2.5 py-3.5 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-slate-950 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:file:text-slate-50 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
+const inputVariants = cva(
+  "flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-2.5 py-3.5 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-slate-950 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:file:text-slate-50 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+  {
+    variants: {
+      customize: {
+        "no-focus": "focus-visible:ring-0",
+      },
+    },
+    defaultVariants: {
+      customize: null,
+    },
   },
 );
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, customize, type, ...props }, ref) => {
+  return <input type={type} className={cn(inputVariants({ customize, className }))} ref={ref} {...props} />;
+});
 Input.displayName = "Input";
 
 export { Input };
