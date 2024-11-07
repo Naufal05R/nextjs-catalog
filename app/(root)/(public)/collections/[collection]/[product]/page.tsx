7520a22771ec2ccbc9f7f833ba35d18eeb0b3c36
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-07 18:20:50 +0700

    feat: implement changes

--- app/(root)/(public)/collections/[collection]/[product]/page.tsx
+++ app/(root)/(public)/collections/[collection]/[product]/page.tsx
@@ -66,7 +66,7 @@ const DetailProductPage = async ({ params }: { params: { collection: string; pro
           </dl>
 
           <menu className="mt-8 flex flex-wrap justify-center gap-4">
-            <Button variant={"secondary"} asChild className="flex-1 px-0 shadow-none">
+            <Button variant="secondary" asChild className="flex-1 px-0 shadow-none">
               <Link
                 target="_blank"
                 href={`https://api.whatsapp.com/send?phone=628999812808&text=Permisi%2C%20apakah%20produk%20${product.title.replace(" ", "%20")}%20tersedia%20%F0%9F%91%8B%F0%9F%98%80`}
@@ -82,11 +82,11 @@ const DetailProductPage = async ({ params }: { params: { collection: string; pro
             <CopyButton title="Share Link" />
 
             <fieldset className="flex basis-5/6 items-center gap-2 overflow-visible">
-              <Button variant={"secondary"} className="text-slate-600 shadow-none">
+              <Button variant="secondary" className="text-slate-600 shadow-none">
                 <Minus />
               </Button>
-              <Input className="flex-1 text-center text-slate-600 shadow-none focus-visible:ring-0" />
-              <Button variant={"secondary"} className="text-slate-600 shadow-none">
+              <Input className="flex-1 text-center text-slate-600 shadow-none" customize="no-focus" />
+              <Button variant="secondary" className="text-slate-600 shadow-none">
                 <Plus />
               </Button>
             </fieldset>
