3bf4831999cd9248c30cc491950b8507d312c119
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-07 13:36:38 +0700

    feat: creating new dynamic DataKeys type

--- types/data.ts
+++ types/data.ts
@@ -1,2 +1,5 @@
 export type Data = { [key: string]: unknown };
 export type Dataset = Array<Data | unknown>;
+export type DataKeys<T extends Data> = {
+  [K in keyof T]: K // T[K] extends string ? K : never;
+}[keyof T];
