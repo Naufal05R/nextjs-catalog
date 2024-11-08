98f3dba884bafc6fce81b97c6f5c4cd8dee33815
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-08 10:40:52 +0700

    feat: implement new changes in collections page

--- app/(root)/(public)/collections/page.tsx
+++ app/(root)/(public)/collections/page.tsx
@@ -1,28 +1,11 @@
-import Collection from "@/components/server/Collection";
-import Mapper from "@/components/server/Mapper";
-import { collections } from "@/constants";
-import { getAllCollection } from "@/lib/actions/collection.action";
+import { DynamicCollections } from "@/components/server/Collection";
 
-export default async function CollectionsPage() {
-  const allCollections = await getAllCollection();
-
+export default function CollectionsPage() {
   return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl">Our Collections</h4>
 
-      <ul className="grid grid-cols-12 items-center gap-4">
-        <Mapper
-          data={allCollections!.slice(0, 3) ?? collections}
-          render={({ title, description, slug }) => (
-            <Collection
-              title={title}
-              description={description ?? ""}
-              href={`/collections/${slug}`}
-              classNames={{ wrapper: "col-span-4" }}
-            />
-          )}
-        />
-      </ul>
+      <DynamicCollections />
     </section>
   );
 }
