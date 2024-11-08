73907d89bccdc20be42ce77b5eb4d75df7252fdc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-08 08:29:34 +0700

    feat: adding new card-shadow utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,27 +10,27 @@ const config: Config = {
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		screens: {
-  			xs: '480px'
-  		},
-  		fontFamily: {
-  			display: ["var(--font-playfair-display)"],
-  			body: ["var(--font-outfit)"]
-  		},
-  		colors: {
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		}
-  	}
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+      fontFamily: {
+        display: ["var(--font-playfair-display)"],
+        body: ["var(--font-outfit)"],
+      },
+      colors: {
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
+      },
+    },
   },
   plugins: [
     twAnimate,
@@ -58,6 +58,10 @@ const config: Config = {
         ".no-scrollbar": {
           "-ms-overflow-style": "none",
           "scrollbar-width": "none",
+        },
+        ".card-shadow": {
+          "@apply shadow-[0_0_24px_2px_theme(colors.slate.200/0.5)] hover:shadow-[0_4px_24px_2px_theme(colors.slate.300/0.5)]":
+            {},
         },
       });
     }),
