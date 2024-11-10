4a85f24e65bbfee76dd2e4c154d1a76f7cddee62
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 05:47:10 +0700

    feat: registering all the new scripts

--- lib/seeds/index.ts
+++ lib/seeds/index.ts
@@ -1,2 +1,5 @@
 export { default as collection } from "./collection";
 export { default as category } from "./category";
+export { default as product } from "./product";
+export { default as gallery } from "./gallery";
+export { default as media } from "./media";
