6e62eea2bc010b82cf11aeb792f46eede0d8d4b0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 05:42:43 +0700

    feat: adding media seeder script

---
+++ lib/seeds/media/seeder.ts
@@ -0,0 +1,316 @@
+import { prisma } from "@/lib/prisma";
+
+export const sapphire_ceylon_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36umo0r000bma15g0p3f4f6" },
+    update: {},
+    create: {
+      id: "cm36umo0r000bma15g0p3f4f6",
+      title: "Sapphire Ceylon 1",
+      slug: "sapphire-ceylon-1",
+      name: "000_sapphire-ceylon-1.jpeg",
+      order: 0,
+      galleryId: "cm36umo0o000ama15bfkcepqz",
+    },
+  });
+};
+
+export const sapphire_ceylon_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36umo0r000cma15ncchjr2f" },
+    update: {},
+    create: {
+      id: "cm36umo0r000cma15ncchjr2f",
+      title: "Sapphire Ceylon 2",
+      slug: "sapphire-ceylon-2",
+      name: "001_sapphire-ceylon-2.jpeg",
+      order: 1,
+      galleryId: "cm36umo0o000ama15bfkcepqz",
+    },
+  });
+};
+
+export const sapphire_ceylon_3 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36umo0r000dma1571hes92s" },
+    update: {},
+    create: {
+      id: "cm36umo0r000dma1571hes92s",
+      title: "Sapphire Ceylon 3",
+      slug: "sapphire-ceylon-3",
+      name: "002_sapphire-ceylon-3.jpeg",
+      order: 1,
+      galleryId: "cm36umo0o000ama15bfkcepqz",
+    },
+  });
+};
+
+export const blue_star_sapphire_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36utuy9000jma15bmiia5jl" },
+    update: {},
+    create: {
+      id: "cm36utuy9000jma15bmiia5jl",
+      title: "Blue Star Sapphire 1",
+      slug: "blue-star-sapphire-1",
+      name: "000_blue-star-sapphire-1.jpeg",
+      order: 0,
+      galleryId: "cm36utuy8000ima15zb7s2t7t",
+    },
+  });
+};
+
+export const blue_star_sapphire_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36utuy9000kma15rnqp9wjf" },
+    update: {},
+    create: {
+      id: "cm36utuy9000kma15rnqp9wjf",
+      title: "Blue Star Sapphire 2",
+      slug: "blue-star-sapphire-2",
+      name: "001_blue-star-sapphire-2.jpeg",
+      order: 1,
+      galleryId: "cm36utuy8000ima15zb7s2t7t",
+    },
+  });
+};
+
+export const blue_star_sapphire_3 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm36utuy9000lma15tbrzzyfs" },
+    update: {},
+    create: {
+      id: "cm36utuy9000lma15tbrzzyfs",
+      title: "Blue Star Sapphire 3",
+      slug: "blue-star-sapphire-3",
+      name: "002_blue-star-sapphire-3.jpeg",
+      order: 2,
+      galleryId: "cm36utuy8000ima15zb7s2t7t",
+    },
+  });
+};
+
+export const yellow_sapphire_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39h56ux00055qutid5npg9h" },
+    update: {},
+    create: {
+      id: "cm39h56ux00055qutid5npg9h",
+      title: "Yellow Sapphire 1",
+      slug: "yellow-sapphire-1",
+      name: "000_yellow-sapphire-1.jpeg",
+      order: 0,
+      galleryId: "cm39h56uv00045qutpbqf967j",
+    },
+  });
+};
+
+export const yellow_sapphire_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39h56ux00065qutxm0ndecv" },
+    update: {},
+    create: {
+      id: "cm39h56ux00065qutxm0ndecv",
+      title: "Yellow Sapphire 2",
+      slug: "yellow-sapphire-2",
+      name: "001_yellow-sapphire-2.jpeg",
+      order: 1,
+      galleryId: "cm39h56uv00045qutpbqf967j",
+    },
+  });
+};
+
+export const yellow_sapphire_3 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39h56ux00075qutg8kweo3r" },
+    update: {},
+    create: {
+      id: "cm39h56ux00075qutg8kweo3r",
+      title: "Yellow Sapphire 3",
+      slug: "yellow-sapphire-3",
+      name: "002_yellow-sapphire-3.jpeg",
+      order: 2,
+      galleryId: "cm39h56uv00045qutpbqf967j",
+    },
+  });
+};
+
+export const ruby_burma_star_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hdrk90005107rzzdqps88" },
+    update: {},
+    create: {
+      id: "cm39hdrk90005107rzzdqps88",
+      title: "Ruby Burma Star 1",
+      slug: "ruby-burma-star-1",
+      name: "000_ruby-burma-star-1.jpeg",
+      order: 0,
+      galleryId: "cm39hdrk70004107r25bmaeoo",
+    },
+  });
+};
+
+export const ruby_burma_star_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hdrk90006107r4u38ohkr" },
+    update: {},
+    create: {
+      id: "cm39hdrk90006107r4u38ohkr",
+      title: "Ruby Burma Star 2",
+      slug: "ruby-burma-star-2",
+      name: "001_ruby-burma-star-2.jpeg",
+      order: 1,
+      galleryId: "cm39hdrk70004107r25bmaeoo",
+    },
+  });
+};
+
+export const ruby_burma_star_3 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hdrk90007107rqlk6eguc" },
+    update: {},
+    create: {
+      id: "cm39hdrk90007107rqlk6eguc",
+      title: "Ruby Burma Star 3",
+      slug: "ruby-burma-star-3",
+      name: "002_ruby-burma-star-3.jpeg",
+      order: 2,
+      galleryId: "cm39hdrk70004107r25bmaeoo",
+    },
+  });
+};
+
+export const permata_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hj1u100056xne1y2dgibm" },
+    update: {},
+    create: {
+      id: "cm39hj1u100056xne1y2dgibm",
+      title: "Permata 1",
+      slug: "permata-1",
+      name: "000_permata-1.jpeg",
+      order: 0,
+      galleryId: "cm39hj1tz00046xne7dub4tsa",
+    },
+  });
+};
+
+export const permata_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hj1u100066xnebvns0n44" },
+    update: {},
+    create: {
+      id: "cm39hj1u100066xnebvns0n44",
+      title: "Permata 2",
+      slug: "permata-2",
+      name: "001_permata-2.jpeg",
+      order: 1,
+      galleryId: "cm39hj1tz00046xne7dub4tsa",
+    },
+  });
+};
+
+export const permata_3 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39hj1u100076xnedxqkl66u" },
+    update: {},
+    create: {
+      id: "cm39hj1u100076xnedxqkl66u",
+      title: "Permata 3",
+      slug: "permata-3",
+      name: "002_permata-3.jpeg",
+      order: 2,
+      galleryId: "cm39hj1tz00046xne7dub4tsa",
+    },
+  });
+};
+
+export const display_batu_surgawi = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39humvp000d6xneknmo4r93" },
+    update: {},
+    create: {
+      id: "cm39humvp000d6xneknmo4r93",
+      title: "Display Batu Surgawi",
+      slug: "display-batu-surgawi",
+      name: "000_display-batu-surgawi.jpeg",
+      order: 0,
+      galleryId: "cm39humvo000c6xne8djm036d",
+    },
+  });
+};
+
+export const detail_batu_surgawi = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39humvp000e6xneyc2jiby4" },
+    update: {},
+    create: {
+      id: "cm39humvp000e6xneyc2jiby4",
+      title: "Detail Batu Surgawi",
+      slug: "detail-batu-surgawi",
+      name: "001_detail-batu-surgawi.jpeg",
+      order: 1,
+      galleryId: "cm39humvo000c6xne8djm036d",
+    },
+  });
+};
+
+export const batu_lepas_hijau = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39i2mjp0006la8wp6tzcbec" },
+    update: {},
+    create: {
+      id: "cm39i2mjp0006la8wp6tzcbec",
+      title: "Batu Lepas Hijau",
+      slug: "batu-lepas-hijau",
+      name: "000_batu-lepas-hijau.jpeg",
+      order: 0,
+      galleryId: "cm39i2mjn0005la8wrddcbcss",
+    },
+  });
+};
+
+export const batu_lepas_shine = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm39i2mjp0007la8w7tsvbg02" },
+    update: {},
+    create: {
+      id: "cm39i2mjp0007la8w7tsvbg02",
+      title: "Batu Lepas Shine",
+      slug: "batu-lepas-shine",
+      name: "001_batu-lepas-shine.jpeg",
+      order: 1,
+      galleryId: "cm39i2mjn0005la8wrddcbcss",
+    },
+  });
+};
+
+export const christian_wiediger_1 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm3bc1wbi0006100yu6lmyrus" },
+    update: {},
+    create: {
+      id: "cm3bc1wbi0006100yu6lmyrus",
+      title: "Christian Wiediger 1",
+      slug: "christian-wiediger-1",
+      name: "000_christian-wiediger-1.jpeg",
+      order: 0,
+      galleryId: "cm3bc1wbg0005100ypaz8gr9a",
+    },
+  });
+};
+
+export const christian_wiediger_2 = async () => {
+  await prisma.media.upsert({
+    where: { id: "cm3bc1wbi0007100y732y6bcs" },
+    update: {},
+    create: {
+      id: "cm3bc1wbi0007100y732y6bcs",
+      title: "Christian Wiediger 2",
+      slug: "christian-wiediger-2",
+      name: "001_christian-wiediger-2.jpeg",
+      order: 1,
+      galleryId: "cm3bc1wbg0005100ypaz8gr9a",
+    },
+  });
+};
