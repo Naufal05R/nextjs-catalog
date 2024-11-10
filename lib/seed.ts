58217ed86cc983eb855fc3be1fae1ccc7703d568
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-10 16:53:26 +0700

    feat: adding seeder to main seed

--- lib/seed.ts
+++ lib/seed.ts
@@ -1,9 +1,10 @@
 import { prisma } from "./prisma";
-import { collection } from "./seeds";
+import { category, collection } from "./seeds";
 
 (async () => {
   try {
     await collection();
+    await category();
 
     console.log("All scripts executed successfully!");
     await prisma.$disconnect();
