176374d95d513558217f9881cb54dfa8d280afb0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-10 15:23:02 +0700

    feat: creating execution function

---
+++ lib/seed.ts
@@ -0,0 +1,15 @@
+import { prisma } from "./prisma";
+import { collection } from "./seeds";
+
+(async function main() {
+  try {
+    await collection();
+
+    console.log("All scripts executed successfully!");
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+})();
