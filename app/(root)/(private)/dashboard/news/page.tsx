171908f2e7002a3455ccad71d1d42a7eb1b1d287
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 15:51:55 +0700

    feat: adding EmptyState

--- app/(root)/(private)/dashboard/news/page.tsx
+++ app/(root)/(private)/dashboard/news/page.tsx
@@ -1,3 +1,9 @@
+import { EmptyState } from "@/components/server/Empty";
+
 export default function DashboardNewsPage({}) {
-  return <section className="grid size-full place-items-start">Dashboard News Page</section>;
+  return (
+    <section className="grid size-full place-items-start">
+      <EmptyState title="News is empty, Please Create a new one!" href="/dashboard/products" alt="Write News" />
+    </section>
+  );
 }
