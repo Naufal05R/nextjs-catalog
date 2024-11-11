c5f5d5cabd54dd360cee842306b701e020604b05
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 11:31:33 +0700

    feat: creating new DeprecatedProducts page

---
+++ components/page/DeprecatedProducts.tsx
@@ -0,0 +1,21 @@
+import { getAllProduct } from "@/lib/actions/product.action";
+import React from "react";
+import Mapper from "../server/Mapper";
+
+const DeprecatedProducts = async () => {
+  const deprecatedProducts = await getAllProduct(false, "isReady");
+  return (
+    <div>
+      {deprecatedProducts && !!deprecatedProducts.length ? (
+        <ul>
+          Deprecated Products
+          <Mapper data={deprecatedProducts} render={({ title }) => <li>{title}</li>} />
+        </ul>
+      ) : (
+        "No Products"
+      )}
+    </div>
+  );
+};
+
+export default DeprecatedProducts;
