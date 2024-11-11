5f6a1007932593d38c5e138ae4f75c00c8fa1746
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 16:08:23 +0700

    feat: implement CreateNewsForm to add news page

--- app/(root)/(private)/dashboard/news/add/page.tsx
+++ app/(root)/(private)/dashboard/news/add/page.tsx
@@ -1,3 +1,11 @@
+import { CreateNewsForm } from "@/components/client/Form";
+
 export default function CreateNewsPage() {
-    return <>Create News Page</>
-};
+  return (
+    <section className="size-full p-4">
+      <h4 className="text-2xl font-semibold capitalize">Create news</h4>
+
+      <CreateNewsForm />
+    </section>
+  );
+}
