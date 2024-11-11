179742eb4a6981b204d3a157f37a3e09f3607338
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 13:05:16 +0700

    feat: change and adjust some components

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 import Mapper from "@/components/server/Mapper";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { getCollection } from "@/lib/actions/collection.action";
 import { getAllProduct } from "@/lib/actions/product.action";
-import { Grid2x2Plus } from "lucide-react";
 import { notFound } from "next/navigation";
-import { DashbaordProductCard } from "@/components/server/Product";
+import { CreateProductCard, DashbaordProductCard } from "@/components/server/Product";
 
 export default async function ProductsByCollectionPage({ params }: { params: { collection: string } }) {
   const collectionId = await getCollection(params.collection, "slug").then((collection) => collection?.id);
@@ -35,39 +32,7 @@ export default async function ProductsByCollectionPage({ params }: { params: { c
             }}
           />
 
-          <Card className="relative col-span-12 h-fit min-h-full overflow-hidden sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4">
-            <CardHeader className="invisible">
-              <figure className="aspect-video w-full rounded-md"></figure>
-            </CardHeader>
-            <CardContent className="invisible">
-              <CardTitle className="text-lg font-semibold">Invisible Title</CardTitle>
-              <CardDescription className="mb-2 line-clamp-3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur unde, quidem amet labore quasi
-                laboriosam placeat ut repellendus. Voluptatum velit optio repellat, cum tempora fuga harum aperiam
-                provident voluptate dolorum! Architecto, harum?
-              </CardDescription>
-              <Badge>Invisible Badge</Badge>
-            </CardContent>
-            <CardFooter className="invisible">
-              <div className="flex flex-col">
-                <CardDescription className="w-full">Invisible Price</CardDescription>
-                <CardTitle className="w-full">Invisible Price</CardTitle>
-              </div>
-            </CardFooter>
-            <CardFooter className="invisible">
-              <Button className="flex-1">Invisible Button</Button>
-            </CardFooter>
-
-            <Link
-              href={`/dashboard/products/${params.collection}/add`}
-              className="absolute inset-0 flex size-full flex-col items-center justify-center"
-            >
-              <Grid2x2Plus className="mx-auto" size={64} />
-              <CardTitle className="mt-4 line-clamp-1 whitespace-nowrap text-xl font-semibold">
-                Create new <span className="capitalize">{params.collection}</span>
-              </CardTitle>
-            </Link>
-          </Card>
+          <CreateProductCard collection={params.collection} />
         </ul>
       ) : (
         <article className="flex size-full flex-col items-center justify-center space-y-4">
