cc80313c0718089e38fc86b5ba244ba233c0c0b8
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 15:42:48 +0700

    feat: creating new Empty component

---
+++ components/server/Empty.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+interface EmptyStateProps {
+  title: string;
+  href: string;
+  alt: string;
+}
+
+export const EmptyState = ({ title, href, alt }: EmptyStateProps) => {
+  return (
+    <article className="flex size-full flex-col items-center justify-center space-y-4">
+      <h4 className="text-center text-3xl font-medium">{title}</h4>
+      <Button asChild>
+        <Link href={href}>{alt}</Link>
+      </Button>
+    </article>
+  );
+};
