5069c3752b3e1ff5055ccc05bca178adf39be682
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 10:42:58 +0700

    feat: updating anchor tag element link to next/link

--- components/nav-projects.tsx
+++ components/nav-projects.tsx
@@ -18,6 +18,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
 
 export function NavProjectsPromotions({
   projects,
@@ -35,10 +36,10 @@ export function NavProjectsPromotions({
         {projects.map((item) => (
           <SidebarMenuItem key={item.name}>
             <SidebarMenuButton asChild>
-              <a href={item.url}>
+              <Link href={item.url}>
                 <item.icon />
                 <span>{item.name}</span>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         ))}
@@ -65,10 +66,10 @@ export function NavProjectsDeprecated({
         {projects.map((item) => (
           <SidebarMenuItem key={item.name}>
             <SidebarMenuButton asChild>
-              <a href={item.url}>
+              <Link href={item.url}>
                 <item.icon />
                 <span>{item.name}</span>
-              </a>
+              </Link>
             </SidebarMenuButton>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
