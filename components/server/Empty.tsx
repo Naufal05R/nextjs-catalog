17f69f4cdd872b90eda4b54488c6b1d5e9d684f4
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-11 15:48:05 +0700

    feat: updating EmptyComponent

--- components/server/Empty.tsx
+++ components/server/Empty.tsx
@@ -11,7 +11,7 @@ interface EmptyStateProps {
 export const EmptyState = ({ title, href, alt }: EmptyStateProps) => {
   return (
     <article className="flex size-full flex-col items-center justify-center space-y-4">
-      <h4 className="text-center text-3xl font-medium">{title}</h4>
+      <h4 className="max-w-[350px] text-center text-3xl font-medium">{title}</h4>
       <Button asChild>
         <Link href={href}>{alt}</Link>
       </Button>
