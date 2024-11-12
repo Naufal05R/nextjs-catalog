65caf73ed13bcb0155458b2f25e1bd5d401d27c8
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-12 18:32:31 +0700

    feat: updating image button

--- components/private/mdx-editor.tsx
+++ components/private/mdx-editor.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "@mdxeditor/editor/style.css";
-import type { ForwardedRef } from "react";
+import { useState, type ForwardedRef } from "react";
 import {
   type MDXEditorMethods,
   type MDXEditorProps,
@@ -29,8 +29,151 @@ import {
   linkDialogPlugin,
   CreateLink,
   CodeToggle,
+  usePublisher,
+  insertImage$,
+  Button,
+  ButtonWithTooltip,
+  SingleChoiceToggleGroup,
+  StrikeThroughSupSubToggles,
 } from "@mdxeditor/editor";
 import { cn } from "@/lib/utils";
+import { ImageIcon, ImagePlus } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { Input } from "../ui/input";
+import Image from "next/image";
+import { useToast } from "@/hooks/use-toast";
+
+const AddImage = () => {
+  const insertImage = usePublisher(insertImage$);
+  const [openImageDialog, seOpenImageDialog] = useState(false);
+  const [imgUrl, setImgUrl] = useState({
+    src: "",
+    alt: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+
+  return (
+    <>
+      <Dialog
+        open={openImageDialog}
+        onOpenChange={() => seOpenImageDialog(false)}
+        aria-labelledby="Create Note"
+        aria-describedby="Dialog to create new note"
+      >
+        <ButtonWithTooltip title="Insert image" className="!size-7 !p-1" onClick={() => seOpenImageDialog(true)}>
+          <DialogTrigger>
+            <ImagePlus size={20} />
+          </DialogTrigger>
+        </ButtonWithTooltip>
+        <DialogContent
+          aria-labelledby="Create Note"
+          aria-describedby="Dialog to create new note"
+          className="flex w-full max-w-screen-xs flex-col gap-6 border-none bg-slate-100 px-6 py-9 shadow-md"
+        >
+          <div className="flex flex-col gap-2">
+            <div className="my-2 flex w-full flex-col items-center justify-center">
+              <ImagePlus />
+              <p className="mt-1 text-2xl font-semibold">Add Image</p>
+            </div>
+            {/* <UploadDropzone
+              appearance={{
+                button: "bg-ui-yellow px-4 py-2 text-light-text-primary my-2 font-medium cursor-pointer text-base",
+                allowedContent: "my-2 text-sm",
+              }}
+              className="dark:border-dark-border border-light-border"
+              endpoint="imageUploader"
+              config={{
+                appendOnPaste: true,
+              }}
+              onClientUploadComplete={(res) => {
+                res.forEach((file) => {
+                  insertImage({
+                    src: file.url,
+                    altText: file.name,
+                    title: file.name,
+                  });
+                });
+                toast({ title: `Upload Complete` });
+                seOpenImageDialog(false);
+              }}
+              onUploadError={(error) => {
+                toast({ title: `ERROR! ${error.message}` });
+              }}
+            /> */}
+            <div className="flex w-full justify-center">
+              <Separator className="bg-light-border dark:bg-dark-border my-4 h-[2px] w-[80%]" />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="image_source" className="font-semibold">
+                Source link:
+              </label>
+              <Input
+                value={imgUrl.src}
+                id="image_source"
+                placeholder="Image Link"
+                className="dark:bg-dark-2 bg-light-2 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                onChange={(e) => setImgUrl({ ...imgUrl, src: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="image_alt" className="font-semibold">
+                Image title:
+              </label>
+              <Input
+                value={imgUrl.alt}
+                id="image_alt"
+                placeholder="Image Title"
+                className="dark:bg-dark-2 bg-light-2 border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                onChange={(e) => setImgUrl({ ...imgUrl, alt: e.target.value })}
+              />
+            </div>
+            <div className="flex w-full items-center justify-end gap-2">
+              <Button
+                className="dark:disabled:bg-dark-2/90 dark:bg-dark-2 disabled:bg-light-3 bg-gray-300/90 hover:opacity-90 focus-visible:ring-0 focus-visible:ring-offset-0"
+                onClick={() => seOpenImageDialog(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                disabled={loading || imgUrl.alt.trim().length === 0 || imgUrl.src.trim().length === 0}
+                className="text-dark-text-primary disabled:bg-green-1/90 bg-green-1 hover:opacity-90 focus-visible:ring-0 focus-visible:ring-offset-0"
+                onClick={async () => {
+                  try {
+                    if (imgUrl.alt.trim().length === 0 || imgUrl.src.trim().length === 0) {
+                      return;
+                    }
+                    setLoading(true);
+                    await insertImage({
+                      src: imgUrl.src,
+                      altText: imgUrl.alt,
+                      title: imgUrl.alt,
+                    });
+                    setLoading(false);
+                    seOpenImageDialog(false);
+                  } catch {
+                    setLoading(false);
+                    toast({ title: "Can't Add your Image, try again." });
+                  }
+                }}
+              >
+                {loading ? (
+                  <>
+                    {/* <Image width={28} height={28} src="/icons/loading-circle.svg" alt="Loading" className="mr-2" /> */}
+                    Saving
+                  </>
+                ) : (
+                  <>Save</>
+                )}
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
 
 export default function InitializedMDXEditor({
   editorRef,
@@ -49,7 +192,7 @@ export default function InitializedMDXEditor({
         }),
         headingsPlugin(),
         codeBlockPlugin(),
-        linkDialogPlugin(),
+        linkDialogPlugin({}),
         diffSourcePlugin(),
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
@@ -68,6 +211,10 @@ export default function InitializedMDXEditor({
                   <BoldItalicUnderlineToggles />
                   <Separator />
                 </div>
+                <div className="flex flex-wrap items-center">
+                  <StrikeThroughSupSubToggles />
+                  <Separator />
+                </div>
               </div>
               <div className="flex flex-wrap items-center gap-y-2">
                 <div className="flex flex-wrap items-center">
@@ -81,7 +228,8 @@ export default function InitializedMDXEditor({
               </div>
               <div className="flex flex-wrap items-center gap-y-2">
                 <div className="flex flex-wrap items-center">
-                  <InsertImage />
+                  {/* <InsertImage /> */}
+                  <AddImage /> {/* Add it here */}
                   <CodeToggle />
                   <CreateLink />
                   <InsertThematicBreak />
