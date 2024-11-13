ba1c30b02f139f96d0ef79ffc4a40e05a77b61dc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-13 18:58:36 +0700

    feat: updating props

--- components/client/Editor.tsx
+++ components/client/Editor.tsx
@@ -2,13 +2,18 @@
 
 import { MDXEditorMethods, MDXEditorProps } from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
-import { forwardRef } from "react";
+import { Dispatch, forwardRef, SetStateAction } from "react";
 
 const Editor = dynamic(() => import("../private/mdx-editor"), {
   ssr: false,
 });
 
-export const RichText = forwardRef<MDXEditorMethods, MDXEditorProps>((props, ref) => (
+interface RichTextProps extends MDXEditorProps {
+  blobUrls: Array<string>;
+  setBlobUrls: Dispatch<SetStateAction<Array<string>>>;
+}
+
+export const RichText = forwardRef<MDXEditorMethods, RichTextProps>((props, ref) => (
   <Editor {...props} editorRef={ref} />
 ));
 
