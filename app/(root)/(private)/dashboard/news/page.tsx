dd753b350587d7b030d044084f4afb4495c61c5e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-14 18:33:23 +0700

    feat: updating dashboard news page

--- app/(root)/(private)/dashboard/news/page.tsx
+++ app/(root)/(private)/dashboard/news/page.tsx
@@ -1,9 +1,17 @@
 import { EmptyState } from "@/components/server/Empty";
+import Mapper from "@/components/server/Mapper";
+import { getAllNews } from "@/lib/actions/news.action";
 
-export default function DashboardNewsPage({}) {
+export default async function DashboardNewsPage({}) {
+  const allNews = await getAllNews();
+
   return (
     <section className="grid size-full place-items-start">
-      <EmptyState title="News is empty, Please Create a new one!" href="/dashboard/news/add" alt="Write News" />
+      {!!allNews?.length ? (
+        <Mapper data={allNews} render={({ title }) => <div>{title}</div>} />
+      ) : (
+        <EmptyState title="News is empty, Please Create a new one!" href="/dashboard/news/add" alt="Write News" />
+      )}
     </section>
   );
 }
