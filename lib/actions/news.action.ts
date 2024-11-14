4472225aac662d430b943b83d1d1d1e64a23b8e0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-14 18:16:44 +0700

    feat: connecting to database

--- lib/actions/news.action.ts
+++ lib/actions/news.action.ts
@@ -3,6 +3,7 @@
 import { NewsFormSchema } from "@/schema/news";
 import { getFileMimeTypes, handlingError, initRawData, slugify } from "../utils";
 import { createObject } from "../service";
+import { prisma } from "../prisma";
 
 const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "";
 const S3_ENDPOINT = process.env.NEXT_PUBLIC_S3_ENDPOINT ?? "";
@@ -18,30 +19,40 @@ export const createNews = async (formData: FormData) => {
       const imagesId = data["images.id"];
       let markdown = content;
 
-      if (imagesFile && imagesId) {
-        for (const [index, file] of imagesFile.entries()) {
-          if (file) {
-            const { fileMime } = getFileMimeTypes(file.type);
-            const objectName = `news/${slugify(title)}/${imagesId[index]}.${fileMime}`;
-            const arrayBuffer = await file.arrayBuffer();
-            const objectStream = Buffer.from(arrayBuffer);
-            const objectParams = {
-              bucketName: APP_NAME,
-              objectName,
-              objectStream,
-            };
+      await prisma.$transaction(async (_prisma) => {
+        if (imagesFile && imagesId) {
+          for (const [index, file] of imagesFile.entries()) {
+            if (file) {
+              const { fileMime } = getFileMimeTypes(file.type);
+              const objectName = `news/${slugify(title)}/${imagesId[index]}.${fileMime}`;
+              const arrayBuffer = await file.arrayBuffer();
+              const objectStream = Buffer.from(arrayBuffer);
+              const objectParams = {
+                bucketName: APP_NAME,
+                objectName,
+                objectStream,
+              };
 
-            await createObject(objectParams);
-            markdown = markdown.replace(imagesId[index], `${S3_ENDPOINT}/${objectName}`);
+              await createObject(objectParams);
+              markdown = markdown.replace(imagesId[index], `${S3_ENDPOINT}/${objectName}`);
+            }
           }
+
+          await createObject({
+            bucketName: APP_NAME,
+            objectName: `news/${slugify(title)}/index.md`,
+            objectStream: markdown,
+          });
         }
 
-        await createObject({
-          bucketName: APP_NAME,
-          objectName: `news/${slugify(title)}/index.md`,
-          objectStream: markdown,
+        await _prisma.news.create({
+          data: {
+            title,
+            slug: slugify(title),
+            description,
+          },
         });
-      }
+      });
     } catch (error) {
       handlingError(error);
     }
