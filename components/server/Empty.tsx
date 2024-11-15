ec1df98cb3c0a4f2077940a2466ddcf52f074546
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-15 13:17:35 +0700

    feat: updating Empty component

--- components/server/Empty.tsx
+++ components/server/Empty.tsx
@@ -2,21 +2,48 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-interface EmptyStateProps {
-  title: string;
-  href?: string;
-  alt?: string;
+interface WithButtonProps {
+  href: string;
+  alt: string;
 }
 
-export const EmptyState = ({ title, href, alt }: EmptyStateProps) => {
+export interface EmptyStateBaseProps {
+  title: string;
+  variant: "button" | "blank";
+}
+
+export interface EmptyStateBlankVariantProps extends EmptyStateBaseProps {
+  variant: "blank";
+  props: {};
+}
+
+export interface EmptyStateButtonVariantProps extends EmptyStateBaseProps {
+  variant: "button";
+  props: WithButtonProps;
+}
+
+type EmptyStateProps = EmptyStateBlankVariantProps | EmptyStateButtonVariantProps;
+
+export const EmptyState = ({ title, variant, props }: EmptyStateProps) => {
+  const Component = () => {
+    if (variant === "blank") {
+      return <></>;
+    }
+
+    if (variant === "button") {
+      const { href, alt } = props;
+      return (
+        <Button asChild>
+          <Link href={href || "/"}>{alt || "Click Me!"}</Link>
+        </Button>
+      );
+    }
+  };
+
   return (
     <article className="flex size-full flex-col items-center justify-center space-y-4">
       <h4 className="max-w-[350px] text-center text-3xl font-medium">{title}</h4>
-      {href && (
-        <Button asChild>
-          <Link href={href}>{alt || "Click Me!"}</Link>
-        </Button>
-      )}
+      <Component />
     </article>
   );
 };
