2461f1f85b70c283efd4bd0f5fd60eddb442dc9d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-15 13:58:36 +0700

    feat: simplifying page

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -1,45 +1,9 @@
-import Mapper from "@/components/server/Mapper";
-import { getCollection } from "@/lib/actions/collection.action";
-import { getAllProduct } from "@/lib/actions/product.action";
-import { notFound } from "next/navigation";
-import { CreateProductCard, DashbaordProductCard } from "@/components/server/Product";
-import { EmptyState } from "@/components/server/Empty";
+import { DashboardProductDisplay } from "@/components/server/Product";
 
-export default async function ProductsByCollectionPage({ params }: { params: { collection: string } }) {
-  const collectionId = await getCollection(params.collection, "slug").then((collection) => collection?.id);
-
-  if (!collectionId) return notFound();
-
-  const products = await getAllProduct({ where: { collectionId: collectionId, isReady: true } });
-
+export default function ProductsByCollectionPage({ params }: { params: { collection: string } }) {
   return (
     <section className="grid size-full place-items-start">
-      {!!products?.length ? (
-        <ul className="grid w-full grid-cols-12 gap-4 px-4 pb-16">
-          <Mapper
-            data={products}
-            render={({ gallery, ...product }) => {
-              return (
-                gallery && (
-                  <DashbaordProductCard
-                    {...product}
-                    collection={params.collection}
-                    thumbnail={gallery.medias[0].name}
-                  />
-                )
-              );
-            }}
-          />
-
-          <CreateProductCard collection={params.collection} />
-        </ul>
-      ) : (
-        <EmptyState
-          title="There's no products exist in this collection"
-          href={`/dashboard/products/${params.collection}/add`}
-          alt="Create a New Product"
-        />
-      )}
+      <DashboardProductDisplay isReady collection={params.collection} />
     </section>
   );
 }
