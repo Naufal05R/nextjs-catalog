2baa4f2c5089497d7a2e8e2d1c233cc0f19acfe1
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-15 13:51:21 +0700

    feat: simplify DeprecatedProducts

--- components/page/DeprecatedProducts.tsx
+++ components/page/DeprecatedProducts.tsx
@@ -1,36 +1,10 @@
 import React from "react";
-import Mapper from "@/components/server/Mapper";
-import { getAllProduct } from "@/lib/actions/product.action";
-import { CreateProductCard, DashbaordProductCard } from "@/components/server/Product";
-import { EmptyStateWithButton } from "../server/Empty";
+import { DashboardProductDisplay } from "@/components/server/Product";
 
-const DeprecatedProducts = async () => {
-  const deprecatedProducts = await getAllProduct({ where: { isReady: false } });
+const DeprecatedProducts = () => {
   return (
     <section className="grid size-full place-items-start">
-      {deprecatedProducts && !!deprecatedProducts.length ? (
-        <ul className="grid w-full grid-cols-12 gap-4 px-4 pb-16">
-          <Mapper
-            data={deprecatedProducts}
-            render={({ gallery, collection: { slug }, ...product }) => {
-              return (
-                !!gallery &&
-                !!gallery.medias.length && (
-                  <DashbaordProductCard {...product} collection={slug} thumbnail={gallery.medias[0].name} />
-                )
-              );
-            }}
-          />
-
-          <CreateProductCard />
-        </ul>
-      ) : (
-        <EmptyStateWithButton
-          title="There's no archived products"
-          href="/dashboard/products"
-          alt="Back to collection"
-        />
-      )}
+      <DashboardProductDisplay isReady={false} />
     </section>
   );
 };
