c416c8b1187e56806610f55d72c35ca15fa6cbda
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-15 10:01:14 +0700

    feat: updating news page

--- app/(root)/(public)/news/page.tsx
+++ app/(root)/(public)/news/page.tsx
@@ -1,55 +1,14 @@
 import React from "react";
-import Link from "next/link";
-import Mapper from "@/components/server/Mapper";
 
-import { Image } from "@/components/server/Media";
+import { FrontEndNewsCard } from "@/components/server/News";
 
-import { getAllNews } from "@/lib/actions/news.action";
-
-const NewsPage = async () => {
-  const allNews = await getAllNews();
-
-  return allNews ? (
+const NewsPage = () => {
+  return (
     <section className="pt-8">
       <h4 className="mb-4 text-3xl">News</h4>
 
-      {!!allNews.length ? (
-        <ul className="mt-8 grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-12">
-          <Mapper
-            data={allNews}
-            render={({ title, description, slug }) => (
-              <Link href={`/news/${slug}`} className="group col-span-4">
-                <article className="w-full overflow-hidden rounded bg-inherit group-hover:cursor-pointer">
-                  <Image
-                    src={`/dummy_1.jpg`}
-                    alt={`dummy_1`}
-                    fill
-                    sizes="25vw"
-                    classNames={{
-                      figure: "w-full aspect-video rounded",
-                      image: "group-hover:scale-125 transition-transform duration-500",
-                    }}
-                  />
-
-                  <blockquote>
-                    <h5 className="mt-2 text-xl font-medium">{title}</h5>
-                    <p className="mr-4 mt-4 line-clamp-2 w-full break-words text-sm text-gray-500">{description}</p>
-                  </blockquote>
-
-                  <blockquote className="mt-4 text-right text-xs text-rose-400">
-                    <time>{new Date().toLocaleDateString()}</time>
-                  </blockquote>
-                </article>
-              </Link>
-            )}
-          />
-        </ul>
-      ) : (
-        ""
-      )}
+      <FrontEndNewsCard />
     </section>
-  ) : (
-    <></>
   );
 };
 
