3917f1c1f56444d3b19f93d1fb848acb2c482c94
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-15 10:03:02 +0700

    feat: updating Emty component make href optional

--- components/server/Empty.tsx
+++ components/server/Empty.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 interface EmptyStateProps {
   title: string;
-  href: string;
+  href?: string;
   alt: string;
 }
 
@@ -12,9 +12,11 @@ export const EmptyState = ({ title, href, alt }: EmptyStateProps) => {
   return (
     <article className="flex size-full flex-col items-center justify-center space-y-4">
       <h4 className="max-w-[350px] text-center text-3xl font-medium">{title}</h4>
-      <Button asChild>
-        <Link href={href}>{alt}</Link>
-      </Button>
+      {href && (
+        <Button asChild>
+          <Link href={href}>{alt}</Link>
+        </Button>
+      )}
     </article>
   );
 };
