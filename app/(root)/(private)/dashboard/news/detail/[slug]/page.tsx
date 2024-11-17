042cfebab49b6c0bfca7423aa842052332a3e4b6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-17 21:29:21 +0700

    feat: rendering news data

--- app/(root)/(private)/dashboard/news/detail/[slug]/page.tsx
+++ app/(root)/(private)/dashboard/news/detail/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default function DetailNewsPage() {
-    return <>Detail News Page</>
-};
+import { getNews } from "@/lib/actions/news.action";
+import { notFound } from "next/navigation";
+
+export default async function DetailNewsPage({ params }: { params: { slug: string } }) {
+  const news = await getNews({ where: { slug: params.slug } });
+
+  if (!news) return notFound();
+
+  console.log(news);
+
+  return (
+    <>
+      Detail News Page
+      {JSON.stringify(news)}
+    </>
+  );
+}
