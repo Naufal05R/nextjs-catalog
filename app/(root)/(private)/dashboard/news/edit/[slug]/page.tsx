48b164d3690fa53b9b037fdc56c330142d7e0cce
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-18 11:18:22 +0700

    feat: updating edit page

--- app/(root)/(private)/dashboard/news/edit/[slug]/page.tsx
+++ app/(root)/(private)/dashboard/news/edit/[slug]/page.tsx
@@ -5,14 +5,20 @@ import { notFound } from "next/navigation";
 const S3_ENDPOINT = process.env.NEXT_PUBLIC_S3_ENDPOINT;
 
 export default async function DetailNewsPage({ params }: { params: { slug: string } }) {
-  const news = await getNews({ where: { slug: params.slug } });
+  const news = await getNews({
+    where: params,
+  });
+
+  if (!news) return notFound();
+
+  const { title } = news;
   const markdown = await fetch(`${S3_ENDPOINT}/news/${params.slug}/article.mdx`).then((r) => r.text());
 
   if (!news || !markdown) return notFound();
 
   return (
     <section className="size-full p-4">
-      <h4 className="text-2xl font-semibold capitalize">Update {news.title}</h4>
+      <h4 className="text-2xl font-semibold capitalize">Update {title}</h4>
 
       <EditNewsForm news={news} text={markdown} />
     </section>
