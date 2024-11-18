b940aa46f48f4ce7c5d9c4a7c53f335377c11bc3
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-18 11:12:37 +0700

    feat: creating new updateNews server action

--- lib/actions/news.action.ts
+++ lib/actions/news.action.ts
@@ -6,7 +6,7 @@ import { createObject, deleteObjects } from "../service";
 import { prisma } from "../prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { Prisma } from "@prisma/client";
+import { News, Prisma } from "@prisma/client";
 
 const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "";
 const S3_ENDPOINT = process.env.NEXT_PUBLIC_S3_ENDPOINT ?? "";
@@ -92,6 +92,67 @@ export const createNews = async (formData: FormData) => {
 
       revalidatePath("/", "layout");
       pathname = "/dashboard";
+    } catch (error) {
+      handlingError(error);
+    } finally {
+      redirect(pathname);
+    }
+  } else {
+    console.log(error);
+  }
+};
+
+export const updateNews = async (prevState: { news: News }, formData: FormData) => {
+  const { news } = prevState;
+  const { data, error, success } = NewsFormSchema.safeParse(initRawData(formData));
+
+  if (success) {
+    const { title, description, content } = data;
+    let pathname = `/dashboard/news/edit/${news.id}`;
+
+    try {
+      const imagesFile = data["images.file"];
+      const imagesId = data["images.id"];
+      let markdown = content;
+
+      await prisma.$transaction(async (_prisma) => {
+        if (imagesFile && imagesId) {
+          for (const [index, file] of imagesFile.entries()) {
+            if (file) {
+              const { fileMime } = getFileMimeTypes(file.type);
+              const objectName = `news/${slugify(title)}/${imagesId[index]}.${fileMime}`;
+              const arrayBuffer = await file.arrayBuffer();
+              const objectStream = Buffer.from(arrayBuffer);
+              const objectParams = {
+                bucketName: APP_NAME,
+                objectName,
+                objectStream,
+              };
+
+              await createObject(objectParams);
+              markdown = markdown.replace(imagesId[index], `${S3_ENDPOINT}/${objectName}`);
+            }
+          }
+
+          await createObject({
+            bucketName: APP_NAME,
+            objectName: `news/${slugify(title)}/article.mdx`,
+            objectStream: markdown,
+          });
+        }
+
+        await _prisma.news.update({
+          where: news,
+          data: {
+            title,
+            slug: slugify(title),
+            description,
+          },
+        });
+      });
+
+      revalidatePath("/", "layout");
+      pathname = `/dashboard/news/detail/${news.id}`;
     } catch (error) {
       handlingError(error);
     } finally {
