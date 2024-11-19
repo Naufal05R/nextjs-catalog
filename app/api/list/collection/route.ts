bea05d7478e469d1c7c9884a496faf4977416f9a
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-19 14:20:22 +0700

    feat: updating api route

--- app/api/list/collection/route.ts
+++ app/api/list/collection/route.ts
@@ -1,9 +1,9 @@
-import { prisma } from "@/lib/prisma";
+import { getAllCollection } from "@/lib/actions/collection.action";
 import { handlingError } from "@/lib/utils";
 
 export const GET = async () => {
   try {
-    const collections = await prisma.collection.findMany();
+    const collections = await getAllCollection();
 
     return Response.json({ success: true, data: collections }, { status: 200 });
   } catch (error) {
