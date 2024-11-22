aeb4e796c966da3be58b3bb730fe01805c65034c
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-22 14:34:58 +0700

    feat: implement search component in the component page

--- app/(root)/(public)/search/page.tsx
+++ app/(root)/(public)/search/page.tsx
@@ -1,7 +1,11 @@
+import { Search } from "@/components/client/Search";
+
 export default function SearchPage() {
   return (
     <section className="pt-8">
-      <h4 className="mb-4 text-xl">Search</h4>
+      <h4 className="mb-4 text-xl">Search Everything</h4>
+
+      <Search />
     </section>
   );
 }
