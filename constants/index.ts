019ec65d1efc0fb16531ea8ec4f1f986ec9f5af2
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-22 16:00:27 +0700

    feat: updating constant index

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,14 @@
-// permanent const
+// ### PERMANENT CONST
+
+// ## List of Data
 export { default as currencies } from "./permanent/currency";
 export { default as languages } from "./permanent/language";
 export { default as navigations } from "./permanent/navigation";
 export { default as footers } from "./permanent/footer";
+// ## Constant Variable
+export { MAX_ITEM_PER_PAGE } from "./permanent/variable";
 
-// temporary const
+// ### TEMPORARY CONST
 export { default as collections } from "./temporary/collection";
 export { default as products } from "./temporary/product";
 export { default as testimonials } from "./temporary/testimonial";
