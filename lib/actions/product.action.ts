9edb2a1314ff4f0169f97c2d79c4f6ca08b76ab0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-23 22:00:47 +0700

    fix: revert it back, couldn't fix it

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -349,13 +349,12 @@ export const updateProduct = async (
               ...product,
               slug: slugify(product.title),
               collectionId: _collection.id,
-              tags: {
-                updateMany: {
-                  where: { productId: id },
-                  data: _tags.map(({ id }) => ({ tagId: id })),
-                },
-              },
             },
+          });
+
+          await _prisma.tagsOnProducts.createManyAndReturn({
+            data: _tags.map(({ id }) => ({ tagId: id, productId: _product.id })),
+            skipDuplicates: true,
           });
 
           const _gallery = await _prisma.gallery.update({
