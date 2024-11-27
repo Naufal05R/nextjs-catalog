3ffedae07114393cf2a336c5b0e058f9d9e7d6d0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-27 08:59:05 +0700

    feat: removing unnecessary registered seeder

--- lib/seeds/index.ts
+++ lib/seeds/index.ts
@@ -4,4 +4,3 @@ export { default as product } from "./product";
 export { default as gallery } from "./gallery";
 export { default as media } from "./media";
 export { default as tag } from "./tag";
-export { default as tags_on_products } from "./tags_on_products";
