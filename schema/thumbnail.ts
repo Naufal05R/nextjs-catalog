02f25d343113e2b97c0847bc3f54b9bee6f9b71b
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-29 11:16:01 +0700

    feat: updating thumbnail schema

--- schema/thumbnail.ts
+++ schema/thumbnail.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const ThumbnailSchema = z.object({
   id: z.string().uuid(),
   exts: z.string().min(3, { message: "Not a valid extensions" }).max(4, { message: "Not a valid extensions" }),
-  newsId: z.string().cuid(),
+  newsId: z.string().uuid(),
 });
