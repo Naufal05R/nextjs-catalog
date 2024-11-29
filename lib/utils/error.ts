02d5b3681519e90a7a483457a42719ee13af01c8
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-29 09:47:21 +0700

    feat: updating util function

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,5 +1,3 @@
-export const throwExtensionError = (allowed: readonly string[], ext?: string) => {
-  throw new Error(
-    `Error: unexpected extensions! Extension should be only be ${allowed.join(", ")}. Extension ${ext} is not currently supported!`,
-  );
+export const extensionError = (allowed: readonly string[], ext?: string) => {
+  return `Error: unexpected extensions! Extension should be only be ${allowed.join(", ")}. Extension ${ext} is not currently supported!`;
 };
