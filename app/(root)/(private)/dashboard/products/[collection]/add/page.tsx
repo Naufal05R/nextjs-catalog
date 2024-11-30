3a28a01071c4971ae8b3a246c7a54eeb47bbe739
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-11-30 22:23:58 +0700

    feat: updating the usage of params in nextjs 15

--- app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/add/page.tsx
@@ -1,14 +1,20 @@
 import { CreateProductForm } from "@/components/client/Form";
 import { getAllCategory } from "@/lib/actions/category.action";
 
-export default async function CreateProductPage({ params }: { params: { collection: string } }) {
+interface CreateProductPageProps {
+  params: Promise<{ collection: string }>;
+}
+
+export default async function CreateProductPage({ params }: CreateProductPageProps) {
+  const { collection } = await params;
+
   const categories = await getAllCategory();
 
   return (
     <section className="size-full p-4">
-      <h4 className="text-2xl font-semibold capitalize">Create new {params.collection} product</h4>
+      <h4 className="text-2xl font-semibold capitalize">Create new {collection} product</h4>
 
-      <CreateProductForm collection={params.collection} categories={categories ?? []} />
+      <CreateProductForm collection={collection} categories={categories ?? []} />
     </section>
   );
 }
