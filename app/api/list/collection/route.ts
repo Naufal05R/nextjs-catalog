e7401ee5897cd05097bb6065e1ab38379ed06ce5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-01 16:32:48 +0700

    feat: modifying collection route api

--- app/api/list/collection/route.ts
+++ app/api/list/collection/route.ts
@@ -1,12 +1,11 @@
 import { getAllCollection } from "@/lib/actions/collection.action";
-import { handlingError } from "@/lib/utils";
 
 export const GET = async () => {
   try {
     const collections = await getAllCollection();
 
-    return Response.json({ success: true, data: collections }, { status: 200 });
+    return Response.json(collections, { status: 200 });
   } catch (error) {
-    handlingError(error);
+    return Response.json(error);
   }
 };
