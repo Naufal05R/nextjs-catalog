1c2f755259a9340a378470fe0725e3d547b83f48
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-01 14:36:39 +0700

    feat: redirecting user to dashboard when user is logged in

--- app/(root)/(private)/dashboard/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(root)/(private)/dashboard/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,12 @@
 import { SignInForm } from "@/components/client/Form";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const { userId } = await auth();
+
+  if (userId) redirect("/dashboard");
+
   return (
     <section className="grid h-screen w-screen place-items-center">
       <SignInForm />
