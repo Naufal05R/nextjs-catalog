09aa5048f9198ba885db8c539a507a249055904e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-01 07:34:41 +0700

    feat: building sign in form ui

--- app/(root)/(private)/dashboard/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(root)/(private)/dashboard/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function SignInPage() {
   return (
     <section className="grid h-screen w-screen place-items-center">
-      <fieldset className="rounded-lg bg-inherit p-8 shadow-lg">
+      <fieldset className="flex w-full max-w-sm flex-col gap-4 rounded-lg bg-inherit p-4 shadow-lg">
         <form action="" />
+        <h2 className="mb-8 text-xl uppercase">Legenda Permata</h2>
+        <Label className="flex flex-col gap-2">
+          Username
+          <Input />
+        </Label>
+        <Label className="flex flex-col gap-2">
+          Password
+          <Input />
+        </Label>
+        <Button className="mt-8 w-full">Sign In</Button>
       </fieldset>
     </section>
   );
