b6f556936950f653e2c75ac3462ca7d8bc735b52
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-01 14:44:44 +0700

    feat: redirecting user from `/dashboard` to `/sign-in` when user is logged off

--- app/(root)/(private)/dashboard/(verified)/layout.tsx
+++ app/(root)/(private)/dashboard/(verified)/layout.tsx
@@ -2,8 +2,14 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { PrivateHeader } from "@/components/server/Header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/dashboard/sign-in");
+
   return (
     <SidebarProvider>
       <SignedIn>
