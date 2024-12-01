8f10d41ccd76bcf466f18c570c06355c694e6c65
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-02 05:43:45 +0700

    feat: adding new error util

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,3 +1,7 @@
 export const extensionError = (allowed: readonly string[], ext?: string) => {
   return `Error: unexpected extensions! Extension should be only be ${allowed.join(", ")}. Extension ${ext} is not currently supported!`;
 };
+
+export const resourceError = (resource?: string) => {
+  return `Error: couldn't find resource! Resource ${resource} isn't available!`;
+};
