1ef342c958d5cfc56bfa6ab7fff5c20e08795f48
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-02 18:48:35 +0700

    feat: modifying metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Playfair_Display, Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ?? "";
+
 const playfair_display = Playfair_Display({
   variable: "--font-playfair-display",
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -17,8 +19,30 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "Legenda Permata",
+  title: {
+    default: "Legenda Permata",
+    template: `%s | Legenda Permata`,
+  },
   description: "Legenda Permata official online store",
+  metadataBase: new URL(SERVER_URL),
+  keywords: [
+    "Legenda Permata",
+    "Permata Legenda",
+    "Toko Permata",
+    "Toko Cincin",
+    "Toko Batu Cincin",
+    "Batu Cincin",
+    "Batu Mulia",
+    "Logam Mulia",
+    "Cincin Permata",
+    "Permata",
+    "Cincin",
+    "Toko Idozz",
+  ],
+  openGraph: {
+    description:
+      "Legenda Permata adalah toko perhiasan online terpercaya yang menawarkan koleksi eksklusif perhiasan berlian, emas, dan permata berkualitas tinggi.",
+  },
 };
 
 export default function RootLayout({
