521800dff67a3000cda266842a46f21fa0e21e74
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-02 15:55:08 +0700

    feat: implement contact feature

--- components/mail/Contact.tsx
+++ components/mail/Contact.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Avatar } from "../ui/avatar";
-import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Img, Link, Section, Tailwind } from "@react-email/components";
 
 interface EmailTemplateProps {
   name: string;
@@ -10,28 +9,29 @@ interface EmailTemplateProps {
 }
 
 export const ContactEmail: React.FC<Readonly<EmailTemplateProps>> = ({ name, email, phone, message }) => (
-  <section className="flex size-full flex-col text-left">
-    <h2 className="mb-8 bg-slate-100 p-8 text-center text-5xl font-black uppercase">LEGENDA PERMATA</h2>
+  <Tailwind>
+    <Section className="flex size-full flex-col text-left">
+      <h2 className="mb-8 mt-0 bg-slate-100 p-8 text-center text-5xl font-black uppercase">LEGENDA PERMATA</h2>
 
-    <article className="p-8">
-      <h3 className="mb-4 text-2xl font-semibold">New message from {name}!</h3>
-      <h4 className="text-xl text-slate-600">Email: {email}</h4>
-      <h4 className="text-xl text-slate-600">Phone: {phone}</h4>
+      <article className="p-8">
+        <h3 className="mb-4 text-2xl font-semibold">New message from {name}!</h3>
+        <h4 className="text-xl text-slate-600">Email: {email}</h4>
+        <h4 className="text-xl text-slate-600">Phone: {phone}</h4>
 
-      <p className="mt-16">{message}</p>
-    </article>
+        <p className="mt-16">{message}</p>
+      </article>
 
-    <hr className="mx-8" />
+      <hr className="mx-8" />
 
-    <article className="flex items-center gap-4 p-8">
-      <Avatar className="size-12 rounded-full">
-        <AvatarImage src="/avatars/shadcn.jpg" alt={name} />
-        <AvatarFallback className="grid size-full place-items-center bg-slate-200">CN</AvatarFallback>
-      </Avatar>
-      <blockquote className="flex h-full flex-col text-sm">
-        <p className="font-semibold">{name}</p>
-        <p className="text-slate-600">{email}</p>
-      </blockquote>
-    </article>
-  </section>
+      <article className="flex items-center gap-4 p-8">
+        <Link className="inline-flex size-12 overflow-hidden rounded-full" href="#">
+          <Img alt="X" src="https://github.com/shadcn.png" style={{ height: "48px", width: "48px" }} />
+        </Link>
+        <blockquote className="m-0 flex h-full flex-col text-sm">
+          <p className="m-0 font-semibold">{name}</p>
+          <p className="m-0 text-slate-600">{email}</p>
+        </blockquote>
+      </article>
+    </Section>
+  </Tailwind>
 );
