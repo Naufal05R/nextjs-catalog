b02cc429e6b37eaf3a7a54ac66bdca46965972d9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-02 09:50:39 +0700

    feat: updating useThumbnail custom hooks

--- hooks/use-thumbnail.ts
+++ hooks/use-thumbnail.ts
@@ -1,12 +1,12 @@
-import { ThumbnailIdentifier } from "@/types/thumbnail";
+import { ACCEPTED_IMAGE_EXTS } from "@/schema/media";
 import useSWR from "swr";
 
-export function useThumbnail({ newsId, resourceId, exts }: ThumbnailIdentifier) {
+export function useThumbnail({ news, exts }: { news: string; exts: (typeof ACCEPTED_IMAGE_EXTS)[number] }) {
   const {
     data: thumbnail,
     error,
     isLoading,
-  } = useSWR<File>(`/api/item/news/${newsId}/thumbnail/${resourceId}?exts=${exts}`, async (key: string) => {
+  } = useSWR<File>(`/api/item/news/${news}/thumbnail?exts=${exts}`, async (key: string) => {
     const blob = await fetch(key).then((r) => r.blob());
     return new File([blob], "thumbnail", { type: blob.type });
   });
