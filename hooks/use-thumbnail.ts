f2d0142d2698a59ed63a8d3d447dd9f7b816a8bf
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-02 09:53:51 +0700

    feat: changing parameter name to be more semantics

--- hooks/use-thumbnail.ts
+++ hooks/use-thumbnail.ts
@@ -6,8 +6,8 @@ export function useThumbnail({ news, exts }: { news: string; exts: (typeof ACCEP
     data: thumbnail,
     error,
     isLoading,
-  } = useSWR<File>(`/api/item/news/${news}/thumbnail?exts=${exts}`, async (key: string) => {
-    const blob = await fetch(key).then((r) => r.blob());
+  } = useSWR<File>(`/api/item/news/${news}/thumbnail?exts=${exts}`, async (url: string) => {
+    const blob = await fetch(url).then((r) => r.blob());
     return new File([blob], "thumbnail", { type: blob.type });
   });
 
