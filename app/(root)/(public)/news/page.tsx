be57b0a06c8a5e24ffb7d1232b034d3d3300b12e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-03 11:30:04 +0700

    feat: adding metadata to the news route

--- app/(root)/(public)/news/page.tsx
+++ app/(root)/(public)/news/page.tsx
@@ -1,6 +1,29 @@
 import React from "react";
-
 import { FrontEndNewsDisplay } from "@/components/server/News";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "News",
+  description: "Legenda Permata official update news",
+  keywords: [
+    "Legenda Permata",
+    "Permata Legenda",
+    "Berita Permata",
+    "Berita Cincin",
+    "Berita Batu Cincin",
+    "Batu Cincin",
+    "Batu Mulia",
+    "Logam Mulia",
+    "Cincin Permata",
+    "Permata",
+    "Cincin",
+    "Berita Idozz",
+  ],
+  openGraph: {
+    description:
+      "Legenda Permata adalah toko perhiasan online terpercaya yang menawarkan koleksi eksklusif perhiasan berlian, emas, dan permata berkualitas tinggi.",
+  },
+};
 
 const NewsPage = () => {
   return (
