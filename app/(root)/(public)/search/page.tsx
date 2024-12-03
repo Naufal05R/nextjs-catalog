f1ebd5f47615e7b1f492d82c922c8dc8234d9526
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-03 10:12:55 +0700

    feat: adding new metadata

--- app/(root)/(public)/search/page.tsx
+++ app/(root)/(public)/search/page.tsx
@@ -1,10 +1,15 @@
 import { Search } from "@/components/client/Search";
 import { SearchProductResult } from "@/components/server/Product";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
 interface SearchPageProps {
   searchParams?: Promise<{ query?: string; page?: number }>;
 }
+
+export const metadata: Metadata = {
+  title: "Search",
+};
 
 export default async function SearchPage({ searchParams }: SearchPageProps) {
   const { /* page, */ query } = (await searchParams) ?? {};
