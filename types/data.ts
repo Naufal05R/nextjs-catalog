d1355901c230efcc8f89d3d3e11f103b2b35e390
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-04 15:19:52 +0700

    feat: updating global type data

--- types/data.ts
+++ types/data.ts
@@ -1,5 +1,7 @@
-export type Data = { [key: string]: unknown };
-export type Dataset = Array<Data | unknown>;
+export type Data = {
+  [key in "title" | "slug" | (string & {})]: unknown;
+};
+export type Dataset<T extends Data = Data> = Array<T extends infer Data ? Data : never | unknown>;
 export type DataKeys<T extends Data> = {
-  [K in keyof T]: K // T[K] extends string ? K : never;
+  [K in keyof T]: K; // T[K] extends string ? K : never;
 }[keyof T];
