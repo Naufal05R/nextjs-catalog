e3703ddc68c7733cae6ad2f546e0a910e84c00e5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-04 12:39:05 +0700

    feat: updating hooks function

--- hooks/use-collection.ts
+++ hooks/use-collection.ts
@@ -1,13 +1,25 @@
 import { fetcher } from "@/lib/utils";
 import { Collection } from "@prisma/client";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 
 export function useCollection() {
-  const { data, error, isLoading } = useSWR<Collection[], unknown>("/api/list/collection", fetcher.json);
+  const {
+    data: collections,
+    error,
+    mutate,
+    isLoading,
+  } = useSWR<Collection[], unknown>("/api/list/collection", fetcher.json);
+  const { cache } = useSWRConfig();
+
+  const refresh = () => {
+    cache.delete("/api/list/collection");
+    mutate();
+  };
 
   return {
-    collections: data,
+    collections,
     isLoading,
+    refresh,
     error,
   };
 }
