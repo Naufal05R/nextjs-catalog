2becd6b5c8c6407e07d6e8370f48b88e1daf35df
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-04 12:40:07 +0700

    feat: abstracting separate function component

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Mapper from "./server/Mapper";
 import { Boxes, ChevronRight, Star } from "lucide-react";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import {
@@ -15,24 +16,71 @@ import {
 } from "@/components/ui/sidebar";
 import { CreateCollectionDialog } from "./client/Dialog";
 import { toggleFavoriteCollection } from "@/lib/actions/collection.action";
-import { useActionState, useEffect, useState } from "react";
-import Mapper from "./server/Mapper";
 import { Button } from "./ui/button";
+import { handlingError } from "@/lib/utils";
 import { useCollection } from "@/hooks/use-collection";
-import { Collection } from "@prisma/client";
+
+function NavMainSub() {
+  const { collections, error, refresh, isLoading } = useCollection();
+
+  if (!collections) return;
+  if (error) handlingError(error);
+
+  return (
+    <SidebarMenuSub>
+      {!!collections.length && (
+        <Mapper
+          data={collections}
+          render={({ id, title, slug, isFavorite }) => (
+            <SidebarMenuSubItem key={title} className="relative flex items-center justify-between">
+              <SidebarMenuSubButton asChild className="flex-1">
+                <Link href={`/dashboard/products/${slug}`}>
+                  <span>{title}</span>
+                </Link>
+              </SidebarMenuSubButton>
+
+              <Button
+                variant={null}
+                size="icon"
+                className="ml-2 mr-[-0.5px] grid size-4 place-items-center rounded-full shadow-none hover:bg-transparent disabled:opacity-100"
+                form={`toggle-favorite-collection-${id}`}
+                disabled={isLoading || isFavorite}
+              >
+                <form
+                  id={`toggle-favorite-collection-${id}`}
+                  action={async (formData: FormData) => {
+                    try {
+                      await toggleFavoriteCollection(undefined, formData);
+                      refresh();
+                    } catch (error) {
+                      handlingError(error);
+                    }
+                  }}
+                  className="hidden"
+                />
+                <input
+                  name="id"
+                  type="hidden"
+                  className="hidden"
+                  defaultValue={id}
+                  form={`toggle-favorite-collection-${id}`}
+                  readOnly
+                />
+                <Star fill={isFavorite ? "#f59e0b" : "transparent"} className="text-amber-500" />
+              </Button>
+            </SidebarMenuSubItem>
+          )}
+        />
+      )}
+
+      <SidebarMenuSubItem>
+        <CreateCollectionDialog />
+      </SidebarMenuSubItem>
+    </SidebarMenuSub>
+  );
+}
 
 export function NavMain() {
-  const [state, formAction, isPending] = useActionState(toggleFavoriteCollection, undefined);
-  const [collections, setCollections] = useState<Array<Collection>>([]);
-  const { collections: data, error, isLoading } = useCollection();
-
-  useEffect(() => {
-    if (data) setCollections(data);
-    if (error) console.log(error);
-  }, [data, error, state]);
-
-  if (isLoading || !collections) return;
-
   return (
     <SidebarGroup>
       <SidebarGroupLabel>All Products</SidebarGroupLabel>
@@ -47,44 +95,7 @@ export function NavMain() {
               </SidebarMenuButton>
             </CollapsibleTrigger>
             <CollapsibleContent>
-              <SidebarMenuSub>
-                {!!collections.length && (
-                  <Mapper
-                    data={collections}
-                    render={({ id, title, slug, isFavorite }) => (
-                      <SidebarMenuSubItem key={title} className="relative flex items-center justify-between">
-                        <SidebarMenuSubButton asChild className="flex-1">
-                          <Link href={`/dashboard/products/${slug}`}>
-                            <span>{title}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-
-                        <Button
-                          variant={null}
-                          size="icon"
-                          className="ml-2 mr-[-0.5px] grid size-4 place-items-center rounded-full shadow-none hover:bg-transparent disabled:opacity-100"
-                          form={`toggle-favorite-collection-${id}`}
-                          disabled={isPending || isFavorite}
-                        >
-                          <form id={`toggle-favorite-collection-${id}`} action={formAction} className="hidden" />
-                          <input
-                            name="id"
-                            type="hidden"
-                            className="hidden"
-                            defaultValue={id}
-                            form={`toggle-favorite-collection-${id}`}
-                            readOnly
-                          />
-                          <Star fill={isFavorite ? "#f59e0b" : "transparent"} className="text-amber-500" />
-                        </Button>
-                      </SidebarMenuSubItem>
-                    )}
-                  />
-                )}
-                <SidebarMenuSubItem>
-                  <CreateCollectionDialog list={collections} setList={setCollections} />
-                </SidebarMenuSubItem>
-              </SidebarMenuSub>
+              <NavMainSub />
             </CollapsibleContent>
           </SidebarMenuItem>
         </Collapsible>
