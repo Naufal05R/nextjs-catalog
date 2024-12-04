200fd7320772b55c5a5287d6a7126cd43c0ab1c5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-04 15:33:04 +0700

    feat: running migration

---
+++ prisma/migrations/20241204083238_adding_new_field/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Category` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "_ProductToTag" ADD CONSTRAINT "_ProductToTag_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "_ProductToTag_AB_unique";
