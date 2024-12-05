b61d6684ca22f27cdd6fe45bb5d089e065deddca
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-05 08:52:07 +0700

    fix: fixing ts error

--- types/data.ts
+++ types/data.ts
@@ -1,7 +1,7 @@
 export type Data = {
   [key in "title" | "slug" | (string & {})]: unknown;
 };
-export type Dataset<T extends Data = Data> = Array<T extends infer Data ? Data : never | unknown>;
+export type Dataset<T extends Data | unknown = unknown> = Array<T>;
 export type DataKeys<T extends Data> = {
-  [K in keyof T]: K; // T[K] extends string ? K : never;
+  [K in keyof T]: K;
 }[keyof T];
