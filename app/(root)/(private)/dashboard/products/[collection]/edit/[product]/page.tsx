46a106e5a88aa3610f914870860b6e5eb17891fc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-13 16:30:16 +0700

    feat: remove unused defaultFiles

--- app/(root)/(private)/dashboard/products/[collection]/edit/[product]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/edit/[product]/page.tsx
@@ -1,6 +1,6 @@
 import { EditProductForm } from "@/components/client/Form";
 import { getAllCategory } from "@/lib/actions/category.action";
-import { getProduct, getProductMedia } from "@/lib/actions/product.action";
+import { getProduct } from "@/lib/actions/product.action";
 import { notFound } from "next/navigation";
 
 interface EditProductPageProps {
@@ -16,25 +16,11 @@ export default async function EditProductPage({ params }: EditProductPageProps) 
 
   const categories = await getAllCategory();
 
-  const defaultFiles = await Promise.all(
-    selectedProduct.gallery!.medias.map(async ({ title, name, order }) => {
-      const blob = await getProductMedia({ slug: product, name });
-      if (!blob) throw new Error("Media not found!");
-      const media = new File([blob], name, { type: blob.type });
-      return { title, name, order, media };
-    }),
-  );
-
   return (
     <section className="size-full p-4">
       <h4 className="text-2xl font-semibold capitalize">Edit {selectedProduct.title} product</h4>
 
-      <EditProductForm
-        collection={collection}
-        categories={categories ?? []}
-        product={selectedProduct}
-        defaultFiles={defaultFiles}
-      />
+      <EditProductForm collection={collection} categories={categories ?? []} product={selectedProduct} />
     </section>
   );
 }
