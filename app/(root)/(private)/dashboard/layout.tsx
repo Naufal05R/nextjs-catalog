78717258cd11c283feb0a84f858a057bb044ffd0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-13 10:10:44 +0700

    feat: remove unused auth and redirect

--- app/(root)/(private)/dashboard/layout.tsx
+++ app/(root)/(private)/dashboard/layout.tsx
@@ -2,14 +2,8 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { PrivateHeader } from "@/components/server/Header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const { userId } = await auth();
-
-  if (!userId) redirect("/sign-in");
-
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <SignedIn>
