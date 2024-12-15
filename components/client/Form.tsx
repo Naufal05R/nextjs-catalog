6d71495d297a43d05207d12010993961261007ab
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-15 14:57:01 +0700

    feat: simplify toast

--- components/client/Form.tsx
+++ components/client/Form.tsx
@@ -141,7 +141,7 @@ export function ContactForm() {
   useEffect(() => {
     const matcher = new RegExp(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
     if (typeof state === "string" && matcher.test(state)) {
-      toast({ title: "Success!", description: "Successfully sending message." });
+      toast({ description: "Successfully sending message!" });
     }
   }, [state]);
 
