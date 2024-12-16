0a3f0d50cb3c8c84600f2afa45ea814072e7f148
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-16 20:35:40 +0700

    feat: throwing user to not found page when collection invalid

--- app/(root)/(private)/dashboard/products/[collection]/page.tsx
+++ app/(root)/(private)/dashboard/products/[collection]/page.tsx
@@ -1,4 +1,8 @@
 import { DashboardProductDisplay } from "@/components/server/Product";
+import { DashboardSkeleton } from "@/components/server/Skeleton";
+import { getCollection } from "@/lib/actions/collection.action";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 interface ProductsByCollectionPageProps {
   params: Promise<{ collection: string }>;
@@ -7,9 +11,13 @@ interface ProductsByCollectionPageProps {
 export default async function ProductsByCollectionPage({ params }: ProductsByCollectionPageProps) {
   const { collection } = await params;
 
+  if (!(await getCollection({ where: { slug: collection } }))) return notFound();
+
   return (
     <section className="grid size-full place-items-start">
-      <DashboardProductDisplay isReady collection={collection} />
+      <Suspense fallback={<DashboardSkeleton />}>
+        <DashboardProductDisplay isReady collection={collection} />
+      </Suspense>
     </section>
   );
 }
