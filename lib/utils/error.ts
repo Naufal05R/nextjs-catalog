57245c0e89bbe13a597b4ac145cc2e41b14c266f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-16 14:11:15 +0700

    feat: updating lib error

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,7 +1,9 @@
-export const extensionError = (allowed: readonly string[], ext?: string) => {
-  return `Error: unexpected extensions! Extension should be only be ${allowed.join(", ")}. Extension ${ext} is not currently supported!`;
-};
+import { handlingPrismaErrors } from "../prisma/error";
 
-export const resourceError = (resource?: "article" | "thumbnail" | (string & {})) => {
-  return `Error: couldn't find resource! Resource ${resource} isn't available!`;
-};
+export const serviceError = (error: unknown) => (typeof error === "string" ? error : handlingPrismaErrors(error));
+
+export const extensionError = (allowed: readonly string[], ext?: string) =>
+  `Error: unexpected extensions! Extension should be only be ${allowed.join(", ")}. Extension ${ext} is not currently supported!`;
+
+export const resourceError = (resource?: "article" | "thumbnail" | (string & {})) =>
+  `Error: couldn't find resource! Resource ${resource} isn't available!`;
