628522dd7d21c79d9c0a9377b5887e356a92bfec
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-12-16 20:34:27 +0700

    feat: implement skeleton as a fallback UI

--- app/(root)/(private)/dashboard/news/page.tsx
+++ app/(root)/(private)/dashboard/news/page.tsx
@@ -1,9 +1,13 @@
 import { BackEndNewsDisplay } from "@/components/server/News";
+import { DashboardSkeleton } from "@/components/server/Skeleton";
+import { Suspense } from "react";
 
 export default function DashboardNewsPage({}) {
   return (
     <section className="grid size-full place-items-start">
-      <BackEndNewsDisplay />
+      <Suspense fallback={<DashboardSkeleton />}>
+        <BackEndNewsDisplay />
+      </Suspense>
     </section>
   );
 }
