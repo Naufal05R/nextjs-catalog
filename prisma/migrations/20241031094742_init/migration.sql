f1257f2f6ffa3c5aef4bbcec6e0df93f48e6347f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-02 22:03:30 +0700

    chore(migration): remove unused migrations

--- prisma/migrations/20241031094742_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isSold` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "isSold",
-ADD COLUMN     "isReady" BOOLEAN NOT NULL DEFAULT true;
