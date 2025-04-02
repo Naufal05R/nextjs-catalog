940b4a2753604ae5982af04e4e5be0e97d04806c
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-02 22:05:58 +0700

    feat(seeder): seeding new lainnya collection

--- lib/seeds/collection/seeder.ts
+++ lib/seeds/collection/seeder.ts
@@ -1,20 +1,5 @@
 import { prisma } from "@/lib/prisma";
 
-export const cincin = async () => {
-  await prisma.collection.upsert({
-    where: { id: "cm2x7wgop0000w6g9yk4wa4yv" },
-    update: {},
-    create: {
-      id: "cm2x7wgop0000w6g9yk4wa4yv",
-      title: "Cincin",
-      slug: "cincin",
-      description: "Collections of Cincin",
-    },
-  });
-
-  console.log('Seeding Collection "Cincin" success!');
-};
-
 export const batu = async () => {
   await prisma.collection.upsert({
     where: { id: "cm32yk6pa000fvlqxco3648rv" },
@@ -23,9 +8,39 @@ export const batu = async () => {
       id: "cm32yk6pa000fvlqxco3648rv",
       title: "Batu",
       slug: "batu",
-      description: "Collections of Batu",
+      description: "Koleksi Batu",
     },
   });
 
   console.log('Seeding Collection "Batu" success!');
+};
+
+export const cincin = async () => {
+  await prisma.collection.upsert({
+    where: { id: "cm2x7wgop0000w6g9yk4wa4yv" },
+    update: {},
+    create: {
+      id: "cm2x7wgop0000w6g9yk4wa4yv",
+      title: "Cincin",
+      slug: "cincin",
+      description: "Koleksi Cincin",
+    },
+  });
+
+  console.log('Seeding Collection "Cincin" success!');
+};
+
+export const lainnya = async () => {
+  await prisma.collection.upsert({
+    where: { id: "cm9015slt00012e68snz7ksb2" },
+    update: {},
+    create: {
+      id: "cm9015slt00012e68snz7ksb2",
+      title: "Lainnya",
+      slug: "lainnya",
+      description: "Koleksi Lainnya",
+    },
+  });
+
+  console.log('Seeding Collection "Lainnya" success!');
 };
