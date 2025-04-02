12f53b45181ec61f12e920fced34b69106032414
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-02 22:08:00 +0700

    feat(seeder): executing new emerald_pendant_1 seeder

--- lib/seeds/media/index.ts
+++ lib/seeds/media/index.ts
@@ -3,6 +3,9 @@ import {
   blue_star_sapphire_1,
   blue_star_sapphire_2,
   blue_star_sapphire_3,
+  emerald_pendant_1,
+  emerald_pendant_2,
+  emerald_pendant_3,
   permata_1,
   permata_2,
   permata_3,
@@ -16,6 +19,9 @@ export default async function seeder() {
     await blue_star_sapphire_1();
     await blue_star_sapphire_2();
     await blue_star_sapphire_3();
+    await emerald_pendant_1();
+    await emerald_pendant_2();
+    await emerald_pendant_3();
 
     console.log("Seeding Media success!");
   } catch (error) {
