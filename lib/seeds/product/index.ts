b2fb8b9051121272f32ed892423fb7ab266e1bbd
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-02 22:07:04 +0700

    feat(seeder): executing kalung_keabadian seeder

--- lib/seeds/product/index.ts
+++ lib/seeds/product/index.ts
@@ -1,10 +1,11 @@
 import { handlingError } from "@/lib/utils";
-import { batu_crystal, cincin_surgawi } from "./seeder";
+import { batu_crystal, kalung_keabadian, cincin_surgawi } from "./seeder";
 
 export default async function seeder() {
   try {
     await batu_crystal();
     await cincin_surgawi();
+    await kalung_keabadian();
 
     console.log("Seeding Product success!");
   } catch (error) {
