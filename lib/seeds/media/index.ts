1f5a3b50685d5336929ab6d4a9c7c35010a4ec20
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-03 21:12:50 +0700

    fix(seeder): changing seeder

--- lib/seeds/media/index.ts
+++ lib/seeds/media/index.ts
@@ -6,16 +6,16 @@ import {
   emerald_pendant_1,
   emerald_pendant_2,
   emerald_pendant_3,
-  permata_1,
-  permata_2,
-  permata_3,
+  aquamarine_loose_stone_1,
+  aquamarine_loose_stone_2,
+  aquamarine_loose_stone_3,
 } from "./seeder";
 
 export default async function seeder() {
   try {
-    await permata_1();
-    await permata_2();
-    await permata_3();
+    await aquamarine_loose_stone_1();
+    await aquamarine_loose_stone_2();
+    await aquamarine_loose_stone_3();
     await blue_star_sapphire_1();
     await blue_star_sapphire_2();
     await blue_star_sapphire_3();
