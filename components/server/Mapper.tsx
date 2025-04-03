7ab7d71c77ccb7b13c341ef337ca0f97b57a17c9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-03 22:35:10 +0700

    fix(component): fixing Mapper component

--- components/server/Mapper.tsx
+++ components/server/Mapper.tsx
@@ -3,11 +3,17 @@ import React, { Children } from "react";
 
 interface MapperProps<T extends Dataset> {
   data: T;
+  getKey?: (item: T[number], index: number) => React.Key;
   render: (item: T[number], index: number) => React.ReactNode;
 }
 
-const Mapper = <T extends Dataset>({ data, render }: MapperProps<T>) => {
-  return Children.toArray(data.map((item: T[number], index) => render(item, index)));
+const Mapper = <T extends Dataset>({ data, getKey = (_, index) => index, render }: MapperProps<T>) => {
+  return Children.toArray(
+    data.map((item, index) => {
+      const element = render(item, index);
+      return React.isValidElement(element) && React.cloneElement(element, { key: getKey(item, index) });
+    }),
+  );
 };
 
 export default Mapper;
