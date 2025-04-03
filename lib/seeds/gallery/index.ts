db21a7bd8b80aee9431d4028c681ce38e717481f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-03 22:05:29 +0700

    feat(seeder): update seeder

--- lib/seeds/gallery/index.ts
+++ lib/seeds/gallery/index.ts
@@ -1,9 +1,9 @@
 import { handlingError } from "@/lib/utils";
-import { batu_crystal, cincin_surgawi, kalung_keabadian } from "./seeder";
+import { aquamarine_loose_stone, cincin_surgawi, kalung_keabadian } from "./seeder";
 
 export default async function seeder() {
   try {
-    await batu_crystal();
+    await aquamarine_loose_stone();
     await cincin_surgawi();
     await kalung_keabadian();
 
