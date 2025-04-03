30632f561040234cc61f32fe37d2bf8de9570eec
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-03 21:12:43 +0700

    fix(seeder): changing seeder

--- lib/seeds/product/index.ts
+++ lib/seeds/product/index.ts
@@ -1,9 +1,9 @@
 import { handlingError } from "@/lib/utils";
-import { batu_crystal, kalung_keabadian, cincin_surgawi } from "./seeder";
+import { aquamarine_loose_stone, kalung_keabadian, cincin_surgawi } from "./seeder";
 
 export default async function seeder() {
   try {
-    await batu_crystal();
+    await aquamarine_loose_stone();
     await cincin_surgawi();
     await kalung_keabadian();
 
