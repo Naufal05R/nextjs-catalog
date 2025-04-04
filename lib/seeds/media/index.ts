3640fcefdda15f2d8121eefff65df131f5bd88e4
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-04 07:20:00 +0700

    feat(seeder): update seeder

--- lib/seeds/media/index.ts
+++ lib/seeds/media/index.ts
@@ -12,6 +12,7 @@ import {
   alexandrite_loose_stone_1,
   alexandrite_loose_stone_2,
   alexandrite_loose_stone_3,
+  ruby_loose_stone_1,
 } from "./seeder";
 
 export default async function seeder() {
@@ -22,6 +23,7 @@ export default async function seeder() {
     await alexandrite_loose_stone_1();
     await alexandrite_loose_stone_2();
     await alexandrite_loose_stone_3();
+    await ruby_loose_stone_1();
     await blue_star_sapphire_1();
     await blue_star_sapphire_2();
     await blue_star_sapphire_3();
