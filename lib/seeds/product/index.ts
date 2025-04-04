14351ede8c57dcfd29b2cbabdcbcab46305f2f84
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-04 07:53:28 +0700

    feat(seeder): update seeder

--- lib/seeds/product/index.ts
+++ lib/seeds/product/index.ts
@@ -1,11 +1,19 @@
 import { handlingError } from "@/lib/utils";
-import { aquamarine_loose_stone, kalung_keabadian, cincin_surgawi, alexandrite_loose_stone, ruby_loose_stone } from "./seeder";
+import {
+  aquamarine_loose_stone,
+  kalung_keabadian,
+  cincin_surgawi,
+  alexandrite_loose_stone,
+  ruby_loose_stone,
+  sapphire_loose_stone,
+} from "./seeder";
 
 export default async function seeder() {
   try {
     await aquamarine_loose_stone();
     await alexandrite_loose_stone();
     await ruby_loose_stone();
+    await sapphire_loose_stone();
     await cincin_surgawi();
     await kalung_keabadian();
 
