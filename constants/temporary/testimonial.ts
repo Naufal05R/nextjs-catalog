034409c698a39f0f2d1da9b0e071808ee19c38b6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-05 12:52:49 +0700

    fix(constant): fixing testimonial

--- constants/temporary/testimonial.ts
+++ constants/temporary/testimonial.ts
@@ -3,25 +3,24 @@ import { defineConst } from "@/lib/utils";
 const testimonials = defineConst([
   {
     stars: 5,
-    name: "Emily Toronto",
-    citizen: "Canada",
-    createdAt: "2023-July-10",
-    comment: "Exceptional quality and stunning design, ideal for any occasion. I'm absolutely in love! ✨👌🏻🤍",
+    name: "Ahmad Shafiq",
+    citizen: "Indonesia",
+    createdAt: "10-July-2024",
+    comment: "Kualitas yang luar biasa dan desain yang memukau. Saya benar-benar jatuh cinta! ✨👌🏻🤍",
   },
   {
     stars: 5,
-    name: "Mia Calgary",
-    citizen: "Canada",
-    createdAt: "2023-October-02",
-    comment: "Absolutely beautiful! Just as shown, arrived quickly, and they're so charming.",
+    name: "Joko Sutrisno",
+    citizen: "Indonesia",
+    createdAt: "2-Oktober-2024",
+    comment: "Benar-benar indah! Persis seperti yang ditunjukkan, tiba dengan cepat, dan sangat menawan.",
   },
   {
     stars: 5,
     name: "Gabriella Olivia",
-    citizen: "New York",
-    createdAt: "2023-September-05",
-    comment:
-      "These earrings are fantastic! I needed secure backs for my second holes, and these are the perfect fit—they look adorable!",
+    citizen: "Indonesia",
+    createdAt: "5-September-2024",
+    comment: "Sangat fantastis! Ini adalah karya yang paling sempurna yang pernah saya lihat.",
   },
 ] as const);
 
