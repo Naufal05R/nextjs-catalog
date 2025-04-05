9afb00f0b17102c7a0e7bc75a8eef363ec5eccf9
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-05 09:45:56 +0700

    feat(seeder): update seeder

--- lib/seeds/product/seeder/other-stone.ts
+++ lib/seeds/product/seeder/other-stone.ts
@@ -1,29 +1,29 @@
 import { prisma } from "@/lib/prisma";
 
-const kalung_keabadian = async () => {
+const emerald_pendant_stone = async () => {
   await prisma.product.upsert({
     where: { id: "cm8zzj7fb00002e68gubmouwg" },
     update: {},
     create: {
       id: "cm8zzj7fb00002e68gubmouwg",
-      title: "Kalung Keabadian",
-      slug: "kalung-keabadian",
-      color: "Moss Green",
-      state: "Raja Ampat",
-      width: 11,
+      title: "Liontin Batu Emerald",
+      slug: "liontin-batu-emerald",
+      color: "Hijau Lumut",
+      state: "Zambia",
+      width: 14,
       height: 9,
-      length: 10.5,
-      weight: 24,
-      price: 1150000,
-      discount: 11,
+      length: 14,
+      weight: 28,
+      price: 1750000,
+      discount: 12.5,
       isFavorite: true,
-      description: "This is Kalung Keabadian product coming from Lainnya collection",
+      description: "Liontin Batu Emerald ini memiliki warna hijau lumut yang sangat cantik dan natural.",
       collectionId: "cm9015slt00012e68snz7ksb2",
       categoryId: "cm39i13s00000la8wdjqbz726",
     },
   });
 
-  console.log('Seeding Product "Batu Crystal" success!');
+  console.log('Seeding Product "Liontin Batu Emerald" success!');
 };
 
-export default { kalung_keabadian };
+export default { emerald_pendant_stone };
