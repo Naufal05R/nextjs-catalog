638fd493247944318dbddcbd5cfab22193e95239
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-05 08:51:32 +0700

    feat(seeder): update seeder

--- lib/seeds/media/index.ts
+++ lib/seeds/media/index.ts
@@ -1,8 +1,7 @@
 import { handlingError } from "@/lib/utils";
 import {
-  blue_star_sapphire_1,
+  ruby_ring_stone_1,
   blue_star_sapphire_2,
-  blue_star_sapphire_3,
   emerald_pendant_1,
   emerald_pendant_2,
   emerald_pendant_3,
@@ -40,9 +39,8 @@ export default async function seeder() {
     await emerald_loose_stone_1();
     await emerald_loose_stone_2();
     await emerald_loose_stone_3();
-    await blue_star_sapphire_1();
+    await ruby_ring_stone_1();
     await blue_star_sapphire_2();
-    await blue_star_sapphire_3();
     await emerald_pendant_1();
     await emerald_pendant_2();
     await emerald_pendant_3();
