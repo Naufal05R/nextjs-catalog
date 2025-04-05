46904e92bc067bea885cc40df1aa93d7920f2dfe
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-04-05 08:34:43 +0700

    feat(seeder): update seeder

--- lib/seeds/product/seeder/ring-stone.ts
+++ lib/seeds/product/seeder/ring-stone.ts
@@ -16,7 +16,7 @@ const ruby_ring_stone = async () => {
       weight: 20,
       price: 1000000,
       discount: 11,
-      isFavorite: true,
+      isFavorite: false,
       description: "Cincin Batu Ruby ini memiliki warna merah darah yang sangat cantik dan elegan.",
       collectionId: "cm2x7wgop0000w6g9yk4wa4yv",
       categoryId: "cm32ot82q0003gk84owcim11u",
@@ -52,4 +52,30 @@ const emerald_ring_stone = async () => {
   console.log('Seeding Product "Cincin Batu Emerald" success!');
 };
 
-export default { ruby_ring_stone, emerald_ring_stone };
+const sapphire_ring_stone = async () => {
+  await prisma.product.upsert({
+    where: { id: "cm93jjd8100032e68mkc77xme" },
+    update: {},
+    create: {
+      id: "cm93jjd8100032e68mkc77xme",
+      title: "Cincin Batu Sapphire",
+      slug: "cincin-batu-sapphire",
+      color: "Biru Laut",
+      state: "Madagascar",
+      width: 12,
+      height: 10,
+      length: 12,
+      weight: 19,
+      price: 1250000,
+      discount: 12.5,
+      isFavorite: true,
+      description: "Cincin Batu Sapphire ini memiliki warna biru laut yang gelap dan natural.",
+      collectionId: "cm2x7wgop0000w6g9yk4wa4yv",
+      categoryId: "cm32ooxry0002gk847mqzh1to",
+    },
+  });
+
+  console.log('Seeding Product "Cincin Batu Sapphire" success!');
+};
+
+export default { ruby_ring_stone, emerald_ring_stone, sapphire_ring_stone };
